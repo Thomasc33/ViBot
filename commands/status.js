2aa8b564627d3545ae6148f1c7ada933a560160f
Tcarr9442 <35206339+Tcarr9442@users.noreply.github.com>
2020-05-02 20:39:19 -0400

    v1.1
    added status

---
+++ commands/status.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'status',
+    description: 'returns bot status',
+    async execute(message, args, bot) {
+        const m = await message.channel.send('Pinging...');
+        let latency = m.createdTimestamp - message.createdTimestamp;
+        var embed = new Discord.MessageEmbed()
+            .setColor('#ffffff')
+            .setTitle('ViBot Status')
+            .addField('Ping', `${latency}ms`, true)
+            .addField('Uptime', `${Math.floor(bot.uptime / 1000)} seconds`, true)
+            .setFooter('ViBot v1.1')
+            .setTimestamp(Date.now());
+        m.edit('', embed);
+    }
+}
