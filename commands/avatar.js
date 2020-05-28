13e514cfffca8e00f0137bf1201207c50b5f6f99
Tcarr9442 <35206339+Tcarr9442@users.noreply.github.com>
2020-05-28 17:23:49 -0400

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -7,14 +7,15 @@ module.exports = {
     alias: 'ava',
     role: 'Almost Raid Leader',
     execute(message, args, bot) {
-        if (args.length == 0) return;
-        let member = message.mentions.members.first()
+        let member
+        if (args.length == 0) member = message.member
+        member = message.mentions.members.first()
         if (member == null) member = message.guild.members.cache.get(args[0])
         if (member == null) member = message.guild.members.cache.filter(user => user.nickname != null).find(nick => nick.nickname.replace(/[^a-z|]/gi, '').toLowerCase().split('|').includes(args[0].toLowerCase()));
         let embed = new Discord.MessageEmbed()
             .setColor('#fefefe')
             .setDescription(member)
-            .setImage(member.user.avatarURL())
+            .setImage(member.user.avatarURL({ dynamic: true, size: 4096 }))
         message.channel.send(embed)
     }
 }
