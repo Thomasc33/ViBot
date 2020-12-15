0cdcb3341f7e756cf7bb58bb5b48145ec2871c21
Thomas C <carrt313@gmail.com>
2020-12-15 00:24:03 -0500

    Update setStatus.js

--- commands/setStatus.js
+++ commands/setStatus.js
@@ -3,14 +3,18 @@ module.exports = {
     name: 'setstatus',
     description: 'Sets the bots custom status',
     requiredArgs: 1,
-    role: 'moderator',
+    role: 'developer',
     execute(message, args, bot) {
-        var status = ''
+        if (message.author.id !== '277636691227836419') return
+        let status = ''
         for (let i in args) status = status.concat(` ${args[i]}`)
         if (status == '') return;
         status = status.slice()
         if (status.length > 128) { message.channel.send("Max length is 128"); return; }
         bot.user.setActivity(status).catch(er => ErrorLogger.log(er, bot));
         message.channel.send("Success")
+    },
+    dmExecution(message, args, bot, db, guild) {
+        this.execute(message, args, bot)
     }
 }
