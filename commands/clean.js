a65782006b6dfe8019434f11fb9e756619493c35
Thomas C <carrt313@gmail.com>
2020-12-31 15:45:49 -0500

    Update clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -13,17 +13,17 @@ module.exports = {
         if (message.channel.parent.name.toLowerCase() === settings.categories.veteran) {
             let lounge = message.guild.channels.cache.get(settings.channels.vetlounge);
             let channel = message.guild.channels.cache.find(c => c.name.includes(`${settings.voiceprefixes.vetprefix}${args[0]}`))
-            if (!channel || !lounge) return message.channels.send('I could not find one of the voice channels (channel or lounge)')
+            if (!channel || !lounge) return message.channel.send('I could not find one of the voice channels (channel or lounge)')
             await this.clean(channel, lounge, message, settings)
         } else if (message.channel.parent.name.toLowerCase() === settings.categories.raiding) {
             let lounge = message.guild.channels.cache.get(settings.channels.lounge);
             let channel = message.guild.channels.cache.find(c => c.name.includes(`${settings.voiceprefixes.raidprefix}${args[0]}`))
-            if (!channel || !lounge) return message.channels.send('I could not find one of the voice channels (channel or lounge)')
+            if (!channel || !lounge) return message.channel.send('I could not find one of the voice channels (channel or lounge)')
             await this.clean(channel, lounge, message, settings)
         } else if (message.channel.parent.name.toLowerCase() === settings.categories.raiding) {
             let channel = message.guild.channels.cache.find(c => c.type == 'category' && c.name == settings.categories.event).children.find(c => c.name.includes(args[0]))
             let lounge = message.guild.channels.cache.get(settings.channels.eventlounge)
-            if (!channel || !lounge) return message.channels.send('I could not find one of the voice channels (channel or lounge)')
+            if (!channel || !lounge) return message.channel.send('I could not find one of the voice channels (channel or lounge)')
             await this.clean(channel, lounge, message, settings)
         }
         await message.channel.send("Channel successfully cleaned");
