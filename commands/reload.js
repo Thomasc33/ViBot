4d6cdb779c22c9f850f74e71fc0bd9317fe5d24c
Thomas C <carrt313@gmail.com>
2021-01-03 17:08:26 -0500

    Update reload.js

--- commands/reload.js
+++ commands/reload.js
@@ -2,20 +2,16 @@ const fs = require('fs')
 module.exports = {
     name: 'reload',
     role: 'moderator',
-    description: 'Updates a command without restarting bot',
+    description: 'Reloads all command without restarting bot',
     execute(message, args, bot) {
-        if (!args.length) return message.channel.send(`Please provide a command to reload`);
-        const commandName = args[0].toLowerCase();
-        const command = bot.commands.get(commandName) || bot.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-        if (!command) return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`);
-        delete require.cache[require.resolve(`./${command.name}.js`)];
-        try {
-            const newCommand = require(`./${command.name}.js`);
-            bot.commands.set(newCommand.name, newCommand);
-            message.react('✅')
-        } catch (error) {
-            console.log(error);
-            message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``);
+        let commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+        message.channel.send('Reloading all commands')
+        bot.commands.clear()
+        for (let file of commandFiles) {
+            delete require.cache[require.resolve(`../commands/${file}`)]
+            let command = require(`../commands/${file}`);
+            bot.commands.set(command.name, command);
         }
+        message.channel.send('Done')
     }
 }
