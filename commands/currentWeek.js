0d8afe66b1350846836f85014dc48cfce58aa402
Thomas C <carrt313@gmail.com>
2021-02-09 00:55:19 -0500

    Update currentWeek.js

--- commands/currentWeek.js
+++ commands/currentWeek.js
@@ -132,13 +132,13 @@ module.exports = {
                     else if (embed.description.length + `\n${string}`.length >= 2048) {//change to 2048
                         if (embed.fields.length == 0) embed.addField('-', string)
                         else if (embed.fields[embed.fields.length - 1].value.length + `\n${string}`.length >= 1024) { //change to 1024
-                            if (embed.length + `\n${string}`.length >= 6000) {//change back to 6k
+                            if (embed.length + `\n${string}`.length >= 5975) {//change back to 6k
                                 embeds.push(new Discord.MessageEmbed(embed))
                                 embed.setDescription('None!')
                                 embed.fields = []
                             } else embed.addField('-', string)
                         } else {
-                            if (embed.length + `\n${string}`.length >= 6000) { //change back to 6k
+                            if (embed.length + `\n${string}`.length >= 5975) { //change back to 6k
                                 embeds.push(new Discord.MessageEmbed(embed))
                                 embed.setDescription('None!')
                                 embed.fields = []
