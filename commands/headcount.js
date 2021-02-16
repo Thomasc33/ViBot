4befbbe6c3db3fe79eb3a6d92286120222d4d36b
Thomas C <carrt313@gmail.com>
2021-02-16 13:53:57 -0500

    hc fix

--- commands/headcount.js
+++ commands/headcount.js
@@ -12,98 +12,77 @@ module.exports = {
     args: '<c/v/fsv/event>',
     role: 'eventrl',
     async execute(message, args, bott) {
+        //settings
         let settings = bott.settings[message.guild.id]
-        if (!(message.channel.parent.name.toLowerCase() === 'raiding' || message.channel.parent.name.toLowerCase() === 'veteran raiding' || message.channel.parent.name.toLowerCase() === 'events')) {
-            return message.channel.send("Try again in a correct category");
-        }
-        bot = bott;
-        var textChannel;
-        if (message.channel.parent.name.toLowerCase() === 'veteran raiding') textChannel = message.guild.channels.cache.get(settings.channels.vetstatus)
-        else if (message.channel.parent.name.toLowerCase() === 'raiding') textChannel = message.guild.channels.cache.get(settings.channels.raidstatus)
-        else if (message.channel.parent.name.toLowerCase() === 'events') textChannel = message.guild.channels.cache.get(settings.channels.eventstatus)
+        if (![settings.categories.raiding, settings.categories.event, settings.categories.veteran].includes(message.channel.parent.name.toLowerCase())) return message.channel.send("Try again in a correct category");
+        if (!bot) bot = bott;
 
+        //get raid status
+        var textChannel;
+        if (message.channel.parent.name.toLowerCase() === settings.categories.veteran) textChannel = message.guild.channels.cache.get(settings.channels.vetstatus)
+        else if (message.channel.parent.name.toLowerCase() === settings.categories.raiding) textChannel = message.guild.channels.cache.get(settings.channels.raidstatus)
+        else if (message.channel.parent.name.toLowerCase() === settings.categories.event) textChannel = message.guild.channels.cache.get(settings.channels.eventstatus)
+
+        //normal execution
         if (message.channel.parent.name.toLowerCase() !== 'events') {
+            let afkTemplates = require('../afkTemplates.json')
+            let runType = null;
             switch (args[0].charAt(0).toLowerCase()) {
                 case 'c':
-                    var embed = new Discord.MessageEmbed()
-                        .setColor('#ff0000')
-                        .setTitle(`Headcount for **Cult Run** started by ${message.guild.members.cache.get(message.author.id).nickname}`)
-                        .setDescription(`React with <${botSettings.emote.malus}> to participate.
-                    React with <${botSettings.emote.LostHallsKey}> if you have a key and are willing to pop.
-                    React with <${botSettings.emote.Plane}> if you plan on rushing.`)
-                        .setTimestamp(Date.now());
-                    var embedMessage = await textChannel.send(`${message.guild.roles.cache.get(settings.roles.cultping)} @here`, embed);
-
-                    cultReact(embedMessage);
+                    runType = afkTemplates.cult;
                     break;
                 case 'v':
-                    var embed = new Discord.MessageEmbed()
-                        .setColor('#8c00ff')
-                        .setTitle(`Headcount for **Void Run** started by ${message.guild.members.cache.get(message.author.id).nickname}`)
-                        .setDescription(`React with <${botSettings.emote.voidd}> to participate.
-                React with <${botSettings.emote.LostHallsKey}> if you have a key and are willing to pop.
-                React with <${botSettings.emote.Vial}> if you have a vial.`)
-                        .setTimestamp(Date.now());
-                    var embedMessage = await textChannel.send(`${message.guild.roles.cache.get(settings.roles.voidping)} @here`, embed);
-
-                    voidReact(embedMessage);
+                    runType = afkTemplates.void;
                     break;
                 case 'f':
-                    var embed = new Discord.MessageEmbed()
-                        .setColor('#8c00ff')
-                        .setTitle(`Headcount for **Full-Skip Void** started by ${message.guild.members.cache.get(message.author.id).nickname}`)
-                        .setDescription(`React with <${botSettings.emote.SkipBoi}> to participate.
-                    React with <${botSettings.emote.LostHallsKey}> if you have a key and are willing to pop.
-                    React with <${botSettings.emote.Vial}> if you have a vial.
-                    React with <${botSettings.emote.Brain}> if you are a brain trickster.
-                    React with <${botSettings.emote.Mystic}> if you are a mystic.`)
-                        .setTimestamp(Date.now());
-                    var embedMessage = await textChannel.send(`${message.guild.roles.cache.get(settings.roles.voidping)} @here`, embed);
-
-                    fsvReact(embedMessage);
+                    runType = afkTemplates.fullSkipVoid;
+                    break;
+                case 'x':
+                    runType = afkTemplates.splitCult;
                     break;
                 default:
-                    message.channel.send("Poll Type not recognized. Please try again")
-                    return;
+                    if (message.member.roles.highest.position < message.guild.roles.cache.get(bot.settings[message.guild.id].roles.vetrl).position) return message.channel.send('Run Type Not Recognized')
+                    else runType = await getTemplate(message, afkTemplates, args[0]).catch(er => message.channel.send(`Unable to get template. Error: \`${er}\``))
+                    break;
             }
-        } else {
-            var eventType = args[0]
-            if (!eventFile[eventType]) {
-                message.channel.send("Event type unrecognized. Check ;events and try again")
-                return;
-            }
-            var event = eventFile[eventType]
-            if (!event.enabled) {
-                message.channel.send(`${event.name} is currently disabled.`);
-                return;
-            }
+            if (!runType) eventHC().catch(er => { message.channel.send('Run Type not recognized') })
+        } else eventHC().catch(er => { message.channel.send(er) })
 
-            var embed = new Discord.MessageEmbed()
-                .setColor('#8c00ff')
-                .setTitle(`Headcount for ${event.name} started by ${message.guild.members.cache.get(message.author.id).nickname}`)
-                .setDescription(`React with with emotes below to indicate what you have`)
-                .setTimestamp(Date.now())
-                .addField('Participate', `<${event.portalEmote}>`, true)
-                .addField('Key', `<${event.keyEmote}>`, true);
-            if (event.rushers) embed.addField('Rushers', `<${botSettings.emote.Plane}>`, true)
-            if (event.stun) embed.addField('Stun', `<${botSettings.emote.Collo}>`, true)
-            if (event.ogmur) embed.addField('(P)ogmur', `<${botSettings.emote.Ogmur}>`, true)
-            if (event.fungal) embed.addField('Fungal Tome', `<${botSettings.emote.UTTomeoftheMushroomTribes}>`, true)
-            if (event.mseal) embed.addField('Mseal', `<${botSettings.emote.MarbleSeal}>`, true)
-            if (event.brain) embed.addField('Decoy', `<${botSettings.emote.Brain}>`, true)
-            if (event.stasis) embed.addField('Mystic', `<${botSettings.emote.Mystic}>`, true)
-            if (event.parylize) embed.addField('Paralyze', `<${botSettings.emote.Paralyze}>`, true)
-            if (event.slow) embed.addField('Slow', `<${botSettings.emote.Slow}>`, true)
-            if (event.daze) embed.addField('Daze', `<${botSettings.emote.Qot}>`, true)
-            if (event.curse) embed.addField('Curse', `<${botSettings.emote.Curse}>`, true)
-            if (event.expose) embed.addField('Expose', `<${botSettings.emote.Expose}>`, true)
-            if (event.warrior) embed.addField('Warrior', `<${botSettings.emote.Warrior}>`, true)
-            if (event.paladin) embed.addField('Paladin', `<${botSettings.emote.Paladin}>`, true)
-            if (event.bard) embed.addField('Bard', `<${botSettings.emote.Bard}>`, true)
-            if (event.priest) embed.addField('Priest', `<${botSettings.emote.Priest}>`, true)
-            var embedMessage = await textChannel.send(embed);
+        async function eventHC() {
+            return new Promise(async (res, rej) => {
+                var eventType = args[0]
+                if (!eventFile[eventType]) return rej("Event type unrecognized. Check ;events and try again")
 
-            eventReact(embedMessage, event);
+                var event = eventFile[eventType]
+                if (!event.enabled) return rej(`${event.name} is currently disabled.`);
+
+                var embed = new Discord.MessageEmbed()
+                    .setColor('#8c00ff')
+                    .setTitle(`Headcount for ${event.name} started by ${message.guild.members.cache.get(message.author.id).nickname}`)
+                    .setDescription(`React with with emotes below to indicate what you have`)
+                    .setTimestamp(Date.now())
+                    .addField('Participate', `<${event.portalEmote}>`, true)
+                    .addField('Key', `<${event.keyEmote}>`, true);
+                if (event.rushers) embed.addField('Rushers', `<${botSettings.emote.Plane}>`, true)
+                if (event.stun) embed.addField('Stun', `<${botSettings.emote.Collo}>`, true)
+                if (event.ogmur) embed.addField('(P)ogmur', `<${botSettings.emote.Ogmur}>`, true)
+                if (event.fungal) embed.addField('Fungal Tome', `<${botSettings.emote.UTTomeoftheMushroomTribes}>`, true)
+                if (event.mseal) embed.addField('Mseal', `<${botSettings.emote.MarbleSeal}>`, true)
+                if (event.brain) embed.addField('Decoy', `<${botSettings.emote.Brain}>`, true)
+                if (event.stasis) embed.addField('Mystic', `<${botSettings.emote.Mystic}>`, true)
+                if (event.parylize) embed.addField('Paralyze', `<${botSettings.emote.Paralyze}>`, true)
+                if (event.slow) embed.addField('Slow', `<${botSettings.emote.Slow}>`, true)
+                if (event.daze) embed.addField('Daze', `<${botSettings.emote.Qot}>`, true)
+                if (event.curse) embed.addField('Curse', `<${botSettings.emote.Curse}>`, true)
+                if (event.expose) embed.addField('Expose', `<${botSettings.emote.Expose}>`, true)
+                if (event.warrior) embed.addField('Warrior', `<${botSettings.emote.Warrior}>`, true)
+                if (event.paladin) embed.addField('Paladin', `<${botSettings.emote.Paladin}>`, true)
+                if (event.bard) embed.addField('Bard', `<${botSettings.emote.Bard}>`, true)
+                if (event.priest) embed.addField('Priest', `<${botSettings.emote.Priest}>`, true)
+                var embedMessage = await textChannel.send(embed);
+                await eventReact(embedMessage, event);
+                res()
+            })
         }
     }
 }
@@ -129,38 +108,9 @@ async function eventReact(pingMessage, event) {
     if (event.priest) await pingMessage.react(botSettings.emoteIDs.Priest)
 }
 
-async function cultReact(message) {
-    message.react(botSettings.emote.malus)
-        .then(message.react(botSettings.emote.LostHallsKey))
-        .then(message.react(botSettings.emote.Warrior))
-        .then(message.react(botSettings.emote.Paladin))
-        .then(message.react(botSettings.emote.Knight))
-        .then(message.react(botSettings.emote.UTTomeoftheMushroomTribes))
-        .then(message.react(botSettings.emote.MarbleSeal))
-        .then(message.react(botSettings.emote.Plane))
-        .catch(er => ErrorLogger.log(er, bot));
-}
-async function voidReact(message) {
-    message.react(botSettings.emote.voidd)
-        .then(message.react(botSettings.emote.LostHallsKey))
-        .then(message.react(botSettings.emote.Vial))
-        .then(message.react(botSettings.emote.Warrior))
-        .then(message.react(botSettings.emote.Paladin))
-        .then(message.react(botSettings.emote.Knight))
-        .then(message.react(botSettings.emote.UTTomeoftheMushroomTribes))
-        .then(message.react(botSettings.emote.MarbleSeal))
-        .catch(er => ErrorLogger.log(er, bot));
-}
-async function fsvReact(message) {
-    message.react(botSettings.emote.SkipBoi)
-        .then(message.react(botSettings.emote.LostHallsKey))
-        .then(message.react(botSettings.emote.Vial))
-        .then(message.react(botSettings.emote.Warrior))
-        .then(message.react(botSettings.emote.Paladin))
-        .then(message.react(botSettings.emote.Knight))
-        .then(message.react(botSettings.emote.UTTomeoftheMushroomTribes))
-        .then(message.react(botSettings.emote.MarbleSeal))
-        .then(message.react(botSettings.emote.Brain))
-        .then(message.react(botSettings.emote.Mystic))
-        .catch(err => ErrorLogger.log(er, bot));
+async function getTemplate(message, afkTemplates, runType) {
+    return new Promise(async (res, rej) => {
+        if (afkTemplates[message.author.id] && afkTemplates[message.author.id][runType.toLowerCase()]) return res(afkTemplates[message.author.id][runType.toLowerCase()])
+        else rej(`No templates for user under: ${runType}`)
+    })
 }
