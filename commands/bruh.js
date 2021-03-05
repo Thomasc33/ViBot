b368d11199adbaabdc95932868ac178b8c0a3a89
Thomas C <carrt313@gmail.com>
2021-03-05 03:22:41 -0500

    Update bruh.js

--- commands/bruh.js
+++ commands/bruh.js
@@ -1,7 +1,7 @@
 module.exports = {
     name: 'bruh',
     description: 'Join your vc and say "bruh"',
-    cooldown: 60,
+    cooldown: 10,
     role: 'developer',
     async execute(message, args, bot, db) {
         let channel = message.guild.channels.cache.get(args[0]) || message.member.voice.channel
