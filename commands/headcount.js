329c7c9c698512c2673b41815eb58d05a9f155cd
Thomas Carr <carrt313@gmail.com>
2022-08-14 00:59:52 -0400

    :+1:

--- commands/headcount.js
+++ commands/headcount.js
@@ -70,7 +70,7 @@ module.exports = {
                 let embed = new Discord.MessageEmbed()
                     .setAuthor(`Headcount for ${run.runName} by ${message.member.nickname}`)
                     .setDescription(`${run.headcountEmote ? `React with ${bot.emojis.cache.get(run.headcountEmote)} if you are coming\n` : ''}React with ${bot.emojis.cache.get(run.keyEmoteID)} if you have a key\nOtherwise react with your gear/class choices below`)
-                    .setColor(run.embed.color)
+                    .setColor(run.embed ? run.embed.color : run.color)
                     .setTimestamp()
 
                 if (symbol.charAt(0) == 'a')
@@ -80,101 +80,17 @@ module.exports = {
                 const pingRole = run.pingRole || run.rolePing;
                 const pings = pingRole ? (typeof pingRole != "string" ? pingRole.map(r => `<@&${settings.roles[r]}>`).join(' ') : `<@&${settings.roles[pingRole]}>`) + ' @here' : '@here';
 
-                let m = await channel.send({ content: `${pings}`, embeds: [embed] , components: []})
-                if (runType.headcountEmote)
-                    m.react(runType.headcountEmote)
-                await m.react(runType.keyEmoteID)
-                if (runType.vialReact) await m.react(botSettings.emoteIDs.Vial)
-                for (let i of runType.earlyLocationReacts) await m.react(i.emoteID)
-                for (let i of runType.reacts) await m.react(i)
+                let m = await channel.send({ content: `${pings}`, embeds: [embed], components: [] })
+                if (run.headcountEmote)
+                    m.react(run.headcountEmote)
+                await m.react(run.keyEmoteID)
+                if (run.vialReact) await m.react(botSettings.emoteIDs.Vial)
+                for (let i of run.earlyLocationReacts) await m.react(i.emoteID)
+                for (let i of run.reacts) {
+                    if (isNaN(+i)) await m.react(reactNameToId(i.toLowerCase()))
+                    else await m.react(i)
+                }
             }
-        }
-
-        async function eventHC(event) {
-            return new Promise(async (res, rej) => {
-                if (!event) return rej('No event found')
-                if (!event.enabled) return rej(`${event.name} is currently disabled.`);
-                let channel;
-                if (message.channel.parent.name.toLowerCase() == settings.categories.veteran) channel = message.guild.channels.cache.get(settings.channels.vetstatus)
-                else if (message.channel.parent.name.toLowerCase() == settings.categories.event) channel = message.guild.channels.cache.get(settings.channels.eventstatus)
-                else if (settings.backend.exaltsInRSA && message.channel.parent.name.toLowerCase() == settings.categories.raiding) channel = message.guild.channels.cache.get(isNaN(settings.channels.exaltstatus) ? settings.channels.raidstatus : settings.channels.exaltstatus)
-                if (!channel) return rej('Try a different category to run this under');
-
-                if (event.name.toLowerCase() == 'random') { // to do
-                    var embed = new Discord.MessageEmbed()
-                        .setColor(event.color || '#8c00ff')
-                        .setTitle(`Headcount for ${event.name} started by ${message.guild.members.cache.get(message.author.id).nickname}`)
-                        .setDescription(`React with with emotes below to indicate what you have`)
-                        .setTimestamp(Date.now())
-                        .addField('Participate', `<${event.portalEmote}>`, true)
-                        .addField('Key', `<${event.keyEmote}>`, true);
-                    if (event.rushers) embed.addField('Rushers', `<${botSettings.emote.Plane}>`, true)
-                    if (event.stun) embed.addField('Stun', `<${botSettings.emote.Collo}>`, true)
-                    if (event.ogmur) embed.addField('(P)ogmur', `<${botSettings.emote.Ogmur}>`, true)
-                    if (event.fungal) embed.addField('Fungal Tome', `<${botSettings.emote.UTTomeoftheMushroomTribes}>`, true)
-                    if (event.mseal) embed.addField('Mseal', `<${botSettings.emote.MarbleSeal}>`, true)
-                    if (event.brain) embed.addField('Decoy', `<${botSettings.emote.Brain}>`, true)
-                    if (event.stasis) embed.addField('Mystic', `<${botSettings.emote.Mystic}>`, true)
-                    if (event.parylize) embed.addField('Paralyze', `<${botSettings.emote.Paralyze}>`, true)
-                    if (event.slow) embed.addField('Slow', `<${botSettings.emote.Slow}>`, true)
-                    if (event.daze) embed.addField('Daze', `<${botSettings.emote.Qot}>`, true)
-                    if (event.curse) embed.addField('Curse', `<${botSettings.emote.Curse}>`, true)
-                    if (event.expose) embed.addField('Expose', `<${botSettings.emote.Expose}>`, true)
-                    if (event.warrior) embed.addField('Warrior', `<${botSettings.emote.Warrior}>`, true)
-                    if (event.paladin) embed.addField('Paladin', `<${botSettings.emote.Paladin}>`, true)
-                    if (event.bard) embed.addField('Bard', `<${botSettings.emote.Bard}>`, true)
-                    if (event.priest) embed.addField('Priest', `<${botSettings.emote.Priest}>`, true)
-                    if (event.aether) embed.addField('Aether Orb', `<${botSettings.emote.UTOrbofAether}>`, true)
-                    var embedMessage = await textChannel.send({ content: `@here ${event.rolePing ? `${settings.roles[event.rolePing] ? `<@&${settings.roles[event.rolePing]}>` : ''}` : ''}`, embeds: [embed] });
-                    for (let i of event.earlyLocationReacts) await embedMessage.react(i.emoteID)
-                    await eventReact(embedMessage, event);
-                    res()
-                }
-                else if (event.name.toLowerCase() == 'exalts') {
-                    var embed = new Discord.MessageEmbed()
-                        .setColor('#8c00ff')
-                        .setTitle(`Headcount for ${event.name} started by ${message.guild.members.cache.get(message.author.id).nickname}`)
-                        .setDescription(`React with with emotes below to indicate what you have`)
-                        .setTimestamp(Date.now())
-                        .addField('Nests', `To participate: <:nest:723001215407095899>\nIf you have a key: <:nestK:723001429693956106>`)
-                        .addField('Fungals', `To participate: <:fungal:723001215696240660>\nIf you have a key: <:fungalK:723001429614395402>`)
-                        .addField('Shatters', `To participate: <:shatters:723001214865899532>\nIf you have a key: <:shattersK:723001429903802478>`)
-                    let embedMessage = await channel.send({ content: `@here ${settings.roles.shattsReact ? `<@&${settings.roles.shattsReact}> ` : ''}${settings.roles.fungalReact ? `<@&${settings.roles.fungalReact}> ` : ''}${settings.roles.nestReact? `<@&${settings.roles.nestReact}>` : ''}`, embeds: [embed] })
-                    await embedMessage.react('723001215407095899')
-                    await embedMessage.react('723001215696240660')
-                    await embedMessage.react('723001214865899532')
-                    await embedMessage.react('723001429693956106')
-                    await embedMessage.react('723001429614395402')
-                    await embedMessage.react('723001429903802478')
-                } else {
-                    var embed = new Discord.MessageEmbed()
-                        .setColor('#8c00ff')
-                        .setTitle(`Headcount for ${event.name} started by ${message.guild.members.cache.get(message.author.id).nickname}`)
-                        .setDescription(`React with with emotes below to indicate what you have`)
-                        .setTimestamp(Date.now())
-                        .addField('Participate', `<${event.portalEmote}>`, true)
-                        .addField('Key', `<${event.keyEmote}>`, true);
-                    if (event.rushers) embed.addField('Rushers', `<${botSettings.emote.Plane}>`, true)
-                    if (event.stun) embed.addField('Stun', `<${botSettings.emote.Collo}>`, true)
-                    if (event.ogmur) embed.addField('(P)ogmur', `<${botSettings.emote.Ogmur}>`, true)
-                    if (event.fungal) embed.addField('Fungal Tome', `<${botSettings.emote.UTTomeoftheMushroomTribes}>`, true)
-                    if (event.mseal) embed.addField('Mseal', `<${botSettings.emote.MarbleSeal}>`, true)
-                    if (event.brain) embed.addField('Decoy', `<${botSettings.emote.Brain}>`, true)
-                    if (event.stasis) embed.addField('Mystic', `<${botSettings.emote.Mystic}>`, true)
-                    if (event.parylize) embed.addField('Paralyze', `<${botSettings.emote.Paralyze}>`, true)
-                    if (event.slow) embed.addField('Slow', `<${botSettings.emote.Slow}>`, true)
-                    if (event.daze) embed.addField('Daze', `<${botSettings.emote.Qot}>`, true)
-                    if (event.curse) embed.addField('Curse', `<${botSettings.emote.Curse}>`, true)
-                    if (event.expose) embed.addField('Expose', `<${botSettings.emote.Expose}>`, true)
-                    if (event.warrior) embed.addField('Warrior', `<${botSettings.emote.Warrior}>`, true)
-                    if (event.paladin) embed.addField('Paladin', `<${botSettings.emote.Paladin}>`, true)
-                    if (event.bard) embed.addField('Bard', `<${botSettings.emote.Bard}>`, true)
-                    if (event.priest) embed.addField('Priest', `<${botSettings.emote.Priest}>`, true)
-                    var embedMessage = await channel.send({ content: `@here ${event.rolePing ? `${settings.roles[event.rolePing] ? `<@&${settings.roles[event.rolePing]}>` : ''}` : ''}`, embeds: [embed] });
-                    await eventReact(embedMessage, event);
-                    res()
-                }
-            })
         }
 
         // add indicator that command was a success
@@ -182,24 +98,24 @@ module.exports = {
     }
 }
 
-async function eventReact(pingMessage, event) {
-    await pingMessage.react(event.portalEmojiId)
-    await pingMessage.react(event.keyEmojiId)
-    if (event.rushers) await pingMessage.react(botSettings.emoteIDs.Plane)
-    if (event.stun) await pingMessage.react(botSettings.emoteIDs.Collo)
-    if (event.ogmur) await pingMessage.react(botSettings.emoteIDs.Ogmur)
-    if (event.fungal) await pingMessage.react(botSettings.emoteIDs.UTTomeoftheMushroomTribes)
-    if (event.mseal) await pingMessage.react(botSettings.emoteIDs.MarbleSeal)
-    if (event.brain) await pingMessage.react(botSettings.emoteIDs.brain)
-    if (event.stasis) await pingMessage.react(botSettings.emoteIDs.mystic)
-    if (event.parylize) await pingMessage.react(botSettings.emoteIDs.Paralyze)
-    if (event.slow) await pingMessage.react(botSettings.emoteIDs.Slow)
-    if (event.daze) await pingMessage.react(botSettings.emoteIDs.Qot)
-    if (event.curse) await pingMessage.react(botSettings.emoteIDs.Curse)
-    if (event.expose) await pingMessage.react(botSettings.emoteIDs.Expose)
-    if (event.warrior) await pingMessage.react(botSettings.emoteIDs.Warrior)
-    if (event.paladin) await pingMessage.react(botSettings.emoteIDs.Paladin)
-    if (event.bard) await pingMessage.react(botSettings.emoteIDs.Bard)
-    if (event.priest) await pingMessage.react(botSettings.emoteIDs.Priest)
-    if (event.aether) await pingMessage.react(botSettings.emoteIDs.UTOrbofAether)
+const reactNameToId = react => {
+    switch (react) {
+        case 'rushers': return botSettings.emoteIDs.Plane
+        case 'stun': return botSettings.emoteIDs.Collo
+        case 'ogmur': return botSettings.emoteIDs.Ogmur
+        case 'fungal': return botSettings.emoteIDs.UTTomeoftheMushroomTribes
+        case 'mseal': return botSettings.emoteIDs.MarbleSeal
+        case 'brain': return botSettings.emoteIDs.brain
+        case 'stasis': return botSettings.emoteIDs.mystic
+        case 'parylize': return botSettings.emoteIDs.Paralyze
+        case 'slow': return botSettings.emoteIDs.Slow
+        case 'daze': return botSettings.emoteIDs.Qot
+        case 'curse': return botSettings.emoteIDs.Curse
+        case 'expose': return botSettings.emoteIDs.Expose
+        case 'warrior': return botSettings.emoteIDs.Warrior
+        case 'paladin': return botSettings.emoteIDs.Paladin
+        case 'bard': return botSettings.emoteIDs.Bard
+        case 'priest': return botSettings.emoteIDs.Priest
+        case 'aether': return botSettings.emoteIDs.UTOrbofAether
+    }
 }
