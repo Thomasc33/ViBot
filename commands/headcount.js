2021228f25b23cc89f281da521e642d018ee3c3f
SkepeOriginal <46399778+SkepeOriginal@users.noreply.github.com>
2022-12-09 01:02:33 +0000

    Revert "Reapertism"

--- commands/headcount.js
+++ commands/headcount.js
@@ -5,7 +5,6 @@ const ErrorLogger = require('../lib/logError');
 const afkCheck = require('./afkCheck');
 const eventAfk = require('./eventAfk');
 const eventFile = require('../data/events.json');
-const afkTemplates = require('../afkTemplates.json')
 
 var bot
 module.exports = {
@@ -18,21 +17,16 @@ module.exports = {
         return `**Run Types:**\n*Regular Afk Checks:*\n${afkCheck.getNotes(guildid, member)}\n*Events:*\nSee \`;events\``
     },
     role: 'eventrl',
-    getRunType,
     async execute(message, args, bott) {
         //settings
-        console.log(args)
         let settings = bott.settings[message.guild.id]
         if (![settings.categories.raiding, settings.categories.event, settings.categories.veteran].includes(message.channel.parent.name.toLowerCase())) return message.channel.send("Try again in a correct category");
         if (!bot) bot = bott;
 
-        let symbol = args[0].toLowerCase();
-        let isAdvanced = false;
-        if (symbol[0] == 'a') {
-            isAdvanced = true;
-        }
-        
-        const runType = getRunType(symbol, message.guild.id)
+        let symbol = args[0].charAt(0).toLowerCase();
+        if (symbol == 'a' && args[0].length > 1)
+            symbol += args[0].charAt(1).toLowerCase();
+        const runType = afkCheck.getRunType(symbol, message.guild.id)
         const eventTypes = eventAfk.getMatchingEvents(args[0], eventFile, message.guild.id)
         if (runType && (message.channel.parent.name.toLowerCase() != settings.categories.event || settings.categories.event == settings.categories.raiding)) {
             if (settings.backend.exaltsInRSA)
@@ -81,11 +75,9 @@ module.exports = {
                     .setColor(run.embed ? run.embed.color : run.color)
                     .setTimestamp()
 
-                if (isAdvanced){
-                    embed.setImage(settings.strings.hallsAdvancedReqsImage)
-                    if (embed.data.thumbnail && typeof (embed.data.thumbnail) == 'string') embed.setThumbnail(embed.data.thumbnail)
+                if (symbol.charAt(0) == 'a')
                     embed.data.description += `\n\n**__Advanced Runs__**\nThis is an **advanced run**, meaning there are extended requirements you **MUST** meet. You must be both **__8/8__** and follow the requirements sheet listed in the afk check.\n\nIf you are caught not meeting these requirements, you will be removed from the run and suspended.`;
-                }
+
                 if (message.author.avatarURL()) embed.setAuthor({ name: `Headcount for ${run.runName} by ${message.member.nickname}`, iconURL: message.author.avatarURL() })
                 const pingRole = run.pingRole || run.rolePing;
                 const pings = pingRole ? (typeof pingRole != "string" ? pingRole.map(r => `<@&${settings.roles[r]}>`).join(' ') : `<@&${settings.roles[pingRole]}>`) + ' @here' : '@here';
@@ -105,13 +97,4 @@ module.exports = {
         // add indicator that command was a success
         message.react('✅')
     }
-}
-function getRunType(char, guildid) {
-    for (let i in afkTemplates[guildid]) {
-        if (char.toLowerCase() == afkTemplates[guildid][i].symbol) return afkTemplates[guildid][i];
-        if (afkTemplates[guildid][i].aliases) {
-            if (afkTemplates[guildid][i].aliases.includes(char.toLowerCase())) return afkTemplates[guildid][i];
-        }
-    }
-    return null
 }
