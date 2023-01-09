c10762b2dfc9e76cc1aceef026ed4f2992f0a4ad
SkepeOriginal <46399778+SkepeOriginal@users.noreply.github.com>
2023-01-09 02:17:59 +0000

    Misc Changes
    - Added a new backend option "UseStaticVCForRaiding": this makes the server use static raiding VCs 1-9
    
    - punishments have aliases: [PU, UI, USERINFO]
    
    - setStatus fixed
    
    - setup added stuff
    
    - added emojis

--- commands/test.js
+++ commands/test.js
@@ -6,15 +6,5 @@ module.exports = {
     description: 'Holds testing code',
     guildSpecific: true,
     role: 'developer',
-    async execute(message, args, bot, db) {
-        let embed = new Discord.EmbedBuilder()
-            .setTitle('Confirm Action')
-            .setColor('#FF0000')
-            .setDescription('Testing')
-        await message.channel.send({ embeds: [embed] }).then(async confirmMessage => {
-            if (await confirmMessage.confirmButton(message.author.id)) {
-                console.log(true)
-            } else console.log(false)
-        })
-    }
+    async execute(message, args, bot, db) {}
 }
