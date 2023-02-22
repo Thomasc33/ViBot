02d8697dd369575a16d75c579e23f2c715e618ae
Thomas Carr <carrt313@gmail.com>
2023-02-22 15:58:16 -0500

    Update pull.js

--- commands/pull.js
+++ commands/pull.js
@@ -13,22 +13,28 @@ module.exports = {
      */
     async execute(message, args, bot, db) {
         if (!['277636691227836419', '258286481167220738'].includes(message.author.id)) return;
+
         console.log('Pulling from github')
-        spawn('git', ['pull'])
-            .on('error', (err) => {
-                console.error(`Error running command: ${err}`);
-                message.channel.send(`Error running command: ${err}`);
-            })
-            .on('close', (code) => {
-                console.log(`Command exited with code ${code}`);
-                message.channel.send(`Command exited with code ${code}`);
-            })
-            .stdout.on('data', (data) => {
-                console.log(`Command output: ${data}`);
-            })
-            .stderr.on('data', (data) => {
-                console.error(`Command error: ${data}`);
-                message.channel.send(`Command error: ${data}`);
-            });
+
+        const gitpull = spawn('git', ['pull'])
+
+        gitpull.on('error', (err) => {
+            console.error(`Error running command: ${err}`);
+            message.channel.send(`Error running command: ${err}`);
+        })
+
+        gitpull.on('close', (code) => {
+            console.log(`Command exited with code ${code}`);
+            message.channel.send(`Command exited with code ${code}`);
+        })
+
+        gitpull.stdout.on('data', (data) => {
+            console.log(`Command output: ${data}`);
+        })
+
+        gitpull.stderr.on('data', (data) => {
+            console.error(`Command error: ${data}`);
+            message.channel.send(`Command error: ${data}`);
+        });
     }
 }
