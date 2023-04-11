b2f1cc061a18ba7017a3efc2c64cd313f36ff972
Ed Wu <eddiewwu@gmail.com>
2023-04-11 17:32:59 -0500

    Implemented Slash Command for Purge

--- commands/purge.js
+++ commands/purge.js
@@ -1,11 +1,32 @@
+const SlashArgType = require('discord-api-types/v10').ApplicationCommandOptionType;
+const { slashArg, slashCommandJSON } = require("../utils");
+
 module.exports = {
     name: 'purge',
     description: 'Removes x messages from channel',
     role: 'headeventrl',
-    args: '<x>',
+    args: [slashArg(SlashArgType.Number, 'count', {
+        required: true,
+        description: "Number of messages (Max 100)"
+    }),],
+    getSlashCommandData(guild) {
+        let json = slashCommandJSON(this, guild)
+        return json
+    },
     async execute(message, args, bot) {
-        if (!args[0] || args[0].replace(/[^0-9]+/g, '') == '') { message.channel.send('Please provide a number'); return; }
-        if (parseInt(args[0]) > 100) { message.channel.send('Max is 100 messages'); return; }
-        message.channel.bulkDelete(args[0])
+        let quantity = args[0]
+        if (!quantity || Number.isNaN(quantity)) { 
+            message.channel.send('Please provide a number.')
+            return
+        }
+
+        if (parseInt(quantity) > 100) { 
+            message.channel.send('Max is 100 messages!')
+            return
+        }
+
+        await message.reply('You\'re stinky :)')
+        await message.deleteReply()
+        message.channel.bulkDelete(quantity)
     }
 }
