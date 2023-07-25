26dab73857ae69f21672b6f88be5d359b2519408
Jimgurka <70647310+Jimgurka@users.noreply.github.com>
2023-07-25 18:21:41 +0200

    VAS prefix removal
    -Added a check to see if the member had a role lower than VAS but still had the prefix
    
    -Now correctly removes prefix from users when they have VAS removed

--- commands/test.js
+++ commands/test.js
@@ -6,8 +6,22 @@ module.exports = {
     description: 'Holds testing code. Do not issue command if you do not know what is in it',
     requiredArgs: 0,
     guildspecific: true,
-    role: 'developer',
-    async execute(message, args, bot, db) {
-        message.channel.send("Test")
+    role: 'raider',
+    async execute(message, args, bot, db,) {
+        const member = message.member
+        const guild = message.guild
+        const vetaffiliateID = bot.settings[guild.id].roles.vetaffiliate
+        const vetaffiliate = guild.roles.cache.get(vetaffiliateID)
+        if (member.roles.highest.position == vetaffiliate.position && !member.displayName.startsWith('>')) {
+            const baseName = member.displayName.replace(/^(\W+)/, '')
+            const oldName = member.displayName
+            await member.setNickname(`${'>'}${baseName}`, 'Automatic Nickname Change: User just got Veteran Affiliate Staff as their highest role')
+        }
+        if (member.roles.highest.position < vetaffiliate.position && member.displayName.startsWith('>')) {
+            const baseName = member.displayName.replace(/^(\W+)/, '')
+            const oldName = member.displayName
+            await member.setNickname(`${baseName}`, 'Automatic Nickname Change: User just got Veteran Affiliate Staff as their highest role, but in reverse')
+            console.log("sauron")
+        }
     }
 }
