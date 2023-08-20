d882e0e699e5ae5fb8b180eb66e09fa25c0856d6
Huntifer <huntifer@dvtk.me>
2023-08-20 07:35:55 -0500

    Show free connections and connection ID in ;dbinfo

--- commands/dbinfo.js
+++ commands/dbinfo.js
@@ -1,5 +1,17 @@
 const { botOwners } = require('../settings.json');
 
+function peekMysql2Queue(q) {
+    let i = 0;
+    let elements = [];
+    while (true) {
+        const c = q.peekAt(i)
+        if (c === undefined) break
+        i += 1
+        elements.push(c)
+    }
+    return elements
+}
+
 module.exports = {
     name: 'dbinfo',
     role: 'assistantdev',
@@ -10,17 +22,16 @@ module.exports = {
         }
         const configKeys = ['waitForConnections', 'connectionLimit', 'maxIdle', 'idleTimeout', 'queueLimit']
         configKeys.forEach(k => {config[k] = db.config[k]})
-        message.channel.send(`DB Config:\n\`\`\`\n${JSON.stringify(config, null, 2)}\`\`\``)
-        let i = 0;
-        let connectionInfo = []
-        while (true) {
-            const c = db._allConnections.peekAt(i)
-            if (c === undefined) break
-            i += 1
-            connectionInfo.push({
-                lastActiveTime: `${c.lastActiveTime} (${new Date(c.lastActiveTime).toISOString()}; ${((new Date() - c.lastActiveTime) / 1000) / 60} minutes ago)`
-            })
-        }
-        message.channel.send(`Live connections:\n\`\`\`\n${JSON.stringify(connectionInfo, null, 2)}\`\`\``)
+        await message.channel.send(`DB Config:\n\`\`\`\n${JSON.stringify(config, null, 2)}\`\`\``)
+        let freeConnections = new Set(peekMysql2Queue(db._freeConnections).map(c => c.connectionId))
+        let connectionInfo = peekMysql2Queue(db._allConnections).map(c => {
+            return {
+                lastActiveTime: `${c.lastActiveTime} (${new Date(c.lastActiveTime).toISOString()}; ${((new Date() - c.lastActiveTime) / 1000) / 60} minutes ago)`,
+                connectionId: c.connectionId,
+                isFree: freeConnections.delete(c.connectionId)
+            }
+        })
+        await message.channel.send(`Live connections:\n\`\`\`\n${JSON.stringify(connectionInfo, null, 2)}\`\`\``)
+        if (freeConnections.size != 0) await message.channel.send(`Other free connections: ${freeConnections.values()}`)
     }
 }
