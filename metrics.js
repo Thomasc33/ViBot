158f80b132848e88eedc8de46f4e58b8e1ebdda4
Huntifer <huntifer@dvtk.me>
2023-08-20 20:36:59 -0500

    Add metrics.js

--- metrics.js
+++ metrics.js
@@ -1,20 +1,20 @@
-const {InfluxDB, Point} = require('@influxdata/influxdb-client')
+const { InfluxDB, Point } = require('@influxdata/influxdb-client')
 const Discord = require('discord.js')
 const botSettings = require('./settings.json')
 const process = require('process')
 
-const influxDB = !!botSettings.influxDB ? new InfluxDB({token: botSettings.influxDB.token, url: botSettings.influxDB.url}).getWriteApi('vibot', botSettings.influxDB.bucket, 'ns') : undefined
+const influxDB = botSettings.influxDB ? new InfluxDB({ token: botSettings.influxDB.token, url: botSettings.influxDB.url }).getWriteApi('vibot', botSettings.influxDB.bucket, 'ns') : undefined
 
 // write node resource/cpu/memory usage
 function writeProcessUsage() {
     const point = new Point('node_process')
-    let ru = process.resourceUsage()
+    const ru = process.resourceUsage()
     for (const [key, value] of Object.entries(ru)) {
-      point.intField(key, value)
+        point.intField(key, value)
     }
-    let mu = process.memoryUsage()
+    const mu = process.memoryUsage()
     for (const [key, value] of Object.entries(mu)) {
-      point.intField(key, value)
+        point.intField(key, value)
     }
     point.floatField('uptime', process.uptime())
     if (!influxDB) return
@@ -24,21 +24,21 @@ function writeProcessUsage() {
 const nodeUsageTimer = setInterval(writeProcessUsage, 10000)
 
 function onShutdown() {
-  clearInterval(nodeUsageTimer)
+    clearInterval(nodeUsageTimer)
 }
 
 process.on('exit', onShutdown)
 
 module.exports = {
-    genPoint (measure, message) {
-        let point = new Point(measure)
+    genPoint(measure, message) {
+        const point = new Point(measure)
             .stringField('id', message.id)
-            .tag('type', message instanceof Discord.Message ? "message" : (message instanceof Discord.BaseInteraction ? "interaction" : undefined))
+            .tag('type', message instanceof Discord.Message ? 'message' : (message instanceof Discord.BaseInteraction ? 'interaction' : undefined))
         if (message.inGuild()) {
-            point.tag('guild', message.guild?.name + ':' + message.guildId)
-            point.tag('channel', message.channel?.name + ':' + message.channelId)
+            point.tag('guild', `${message.guild?.name}:${message.guildId}`)
+            point.tag('channel', `${message.channel?.name}:${message.channelId}`)
         } else if (message.channel.isDMBased()) {
-            point.tag('dmuser', message.channel.recipient?.tag + ':' + message.channel.recipientId)
+            point.tag('dmuser', `${message.channel.recipient?.tag}:${message.channel.recipientId}`)
         }
         return point
     },
@@ -50,9 +50,9 @@ module.exports = {
         influxDB.flush()
     },
     logWrapper(measurement, f) {
-        return async function(message, ...argv) {
+        return async (message, ...argv) => {
             let point = module.exports.genPoint(measurement, message)
-            let log = function(tag) {
+            const log = (tag) => {
                 if (tag) point = point.tag(tag, true)
             }
             const ret = await f(log, message, ...argv)
