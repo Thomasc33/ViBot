9355ca35ed2b36631932a3d67e50b2d3a0f98014
Raghav Viswakumar <rv519@ic.ac.uk>
2023-08-29 21:01:51 +0200

    Updated Clean Command
    - Invalid setup values have been removed from clean
    + clean.js has had a minor cleanup with eslint errors fixed
    - .eslintignore no longer has clean.js

--- commands/clean.js
+++ commands/clean.js
@@ -1,34 +1,30 @@
-const Discord = require('discord.js');
-
 module.exports = {
     name: 'clean',
-    description: 'Cleans the voice channel you are in',
-    args: '(channelId)',
+    description: 'Cleans the voice channel you are in or the one specified',
     requiredArgs: 0,
+    args: '(channelID)',
     role: 'eventrl',
     async execute(message, args, bot) {
-        if (!message.member.voice) { return message.channel.send('You are not in any voice channel') }
-        let channel = message.channel
-        let settings = bot.settings[message.guild.id]
-        let lounge = null;
-        if (channel.parent.name.toLowerCase() === settings.categories.raiding) lounge = message.guild.channels.cache.get(settings.voice.lounge)
-        else if (channel.parent.name.toLowerCase() === settings.categories.veteran) lounge = message.guild.channels.cache.get(settings.voice.vetlounge)
-        else lounge = message.guild.channels.cache.get(settings.voice.eventlounge)
-        if (channel.id === settings.channels.accursedcommands) lounge = message.guild.channels.cache.get(settings.voice.vetlounge)
-        if (!lounge) lounge = message.guild.channels.cache.get(settings.voice.lounge)
-        if (!lounge) lounge = message.guild.channels.cache.get(settings.voice.afk)
-        if (!lounge) await message.channel.send('I could not find any lounge to move all of the raiders to')
-        let vc = null;
-        if (args.length > 0) vc = await message.guild.channels.cache.get(args[0]) 
+        const botSettings = bot.settings[message.guild.id]
+        let vc = null
+        if (args.length > 0) vc = await message.guild.channels.cache.get(args[0])
         if (!vc) vc = await message.guild.channels.cache.get(message.member.voice.channelId)
-        if (!vc) return message.channel.send('Something went wrong trying to fetch the channel you are in')
-        await this.clean(vc, lounge, message.guild, settings);
-        await message.channel.send("Channel successfully cleaned");
-    },
-    async clean(voiceChannel, lounge, guild, settings) {
-        voiceChannel.members.each(async member => {
-            if (member.roles.highest.position >= guild.roles.cache.get(settings.roles.eventrl).position) { return }
-            await member.voice.setChannel(lounge, 'cleaning').catch(er => { })
-        })
+        if (!vc) return await message.reply('The channel to clean was not found.')
+
+        let lounge = null
+        if (!lounge) lounge = message.guild.channels.cache.get(botSettings.voice.lounge)
+        if (!lounge) lounge = message.guild.channels.cache.get(botSettings.voice.afk)
+        if (!lounge) return await message.reply('The lounge channel is not defined in the settings.')
+
+        let cleaned = 0
+        const minimumStaffRole = message.guild.roles.cache.get(botSettings.roles.minimumStaffRole)
+        if (!minimumStaffRole) return await message.reply('The minimum staff role is not defined in the settings.')
+        await Promise.all(vc.members.map(async m => {
+            if (m.roles.highest.position < minimumStaffRole.position) {
+                await m.voice.setChannel(lounge).catch(er => {})
+                cleaned++
+            }
+        }))
+        await message.reply(`${cleaned == 0 ? 'The channel is already clean.' : `The channel was successfully cleaned and \`${cleaned}\` members were moved.`}`)
     }
 }
