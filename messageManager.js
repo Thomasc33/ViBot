02fd378a00f30f69b09e767b808847be0789ec81
husky-rotmg <38387144+kerr-andrew@users.noreply.github.com>
2023-10-07 17:25:04 -0400

    messageManager

--- messageManager.js
+++ messageManager.js
@@ -9,17 +9,17 @@ const restarting = require('./commands/restart')
 const verification = require('./commands/verification')
 const stats = require('./commands/stats')
 const modmail = require('./commands/modmail')
-const { argString } = require('./commands/commands.js');
+const { argString } = require('./commands/commands.js')
 const { getDB } = require('./dbSetup.js')
 const { LegacyCommandOptions, LegacyParserError } = require('./utils.js')
 
 class MessageManager {
-    #bot;
-    #botSettings;
-    #prefix;
-    #tokenDB;
-    #cooldowns = new Discord.Collection();
-    #vibotControlGuild;
+    #bot
+    #botSettings
+    #prefix
+    #tokenDB
+    #cooldowns = new Discord.Collection()
+    #vibotControlGuild
 
     constructor(bot, botSettings) {
         this.#bot = bot
@@ -28,7 +28,7 @@ class MessageManager {
         this.#tokenDB = mysql.createConnection(botSettings.tokenDBInfo)
 
         this.#tokenDB.connect(err => {
-            if (err) ErrorLogger.log(err, bot);
+            if (err) ErrorLogger.log(err, bot)
             console.log('Connected to token database')
         })
 
@@ -46,24 +46,24 @@ class MessageManager {
     handleMessage(message) {
         switch (message.channel.type) {
             case Discord.ChannelType.GuildText:
-                if (message.author.bot) return;
+                if (message.author.bot) return
                 if (message.content.startsWith(this.#prefix) && message.content[this.#prefix.length] !== ' ') {
                     // Handle commands (messages that start with a prefix + command)
-                    this.handleCommand(message, false);
+                    this.handleCommand(message, false)
                 } else {
                     // Handle non-command messages
                     this.autoMod(message)
                 }
 
-                break;
+                break
             case Discord.ChannelType.DM:
                 try {
-                    this.dmHandler(message);
+                    this.dmHandler(message)
                 } catch (er) {
-                    ErrorLogger.log(er, this.#bot, message.guild);
+                    ErrorLogger.log(er, this.#bot, message.guild)
                 }
 
-                break;
+                break
             default:
             // Log an error? I guess?
         }
@@ -76,13 +76,12 @@ class MessageManager {
      * @param {Command Module} commmand
      * @returns {Boolean}
      */
-    commandPermitted(member, guild, command) {
+    #commandPermitted(member, guild, command) {
         // All of these are for user readibility, making it much easier for you reading this right now. You're welcome :)
         const memberPosition = member.roles.highest.position
         const settings = this.#bot.settings[guild.id]
         const roleCache = guild.roles.cache
         const memberId = member.id
-
         return (
             // Check command role permission
             (memberPosition >= roleCache.get(settings.roles[command.role]).position)
@@ -94,74 +93,50 @@ class MessageManager {
             || (command.userOverride && command.userOverride.includes(memberId))
             // Check admin override
             || this.#bot.adminUsers.includes(memberId)
-        );
+        )
     }
 
     /**
-     * Runs the command processing pipeline including parsing, state checks, permissions checks, etc.
-     * @param {Message|Interaction} message or interaction
-     * @returns
+     * Handle logging and replying to command errors
+     * @param {Discord.Message | Discord.Interaction} e
+     * @param {string} commandName
+     * @param {string} userMsg
+     * @param {string?} logMsg
      */
-    async handleCommand(e, isInteraction) {
-        if (isInteraction) {
-            if (!this.#bot.settings[e.guild.id]) return
-        }
+    async #commandError(e, commandName, userMsg, logMsg) {
+        if (userMsg) await e.reply(userMsg)
+        logMessage('command', e, (p) => {
+            p.tag('command', commandName)
+            p.tag('errortype', logMsg || userMsg)
+            return p
+        })
+    }
 
-        let commandName, args, argCount;
-        if (!isInteraction) {
-            const [commandNameTmp, ...argsTmp] = e.content.slice(this.#prefix.length).split(/ +/gi)
-            commandName = commandNameTmp
-            args = argsTmp
-            argCount = args.length
-        } else {
-            commandName = e.commandName
-            args = e.getArgs()
-            argCount = e.options.length
-        }
+    async #validateCommand(e, command, commandName, argCount) {
+        if (!command) return await this.#commandError(e, commandName, 'Command doesnt exist, check `commands` and try again', 'does not exist')
 
-        // Get the command
-        const command = this.#bot.commands.get(commandName) || this.#bot.commands.find(cmd => cmd.alias && cmd.alias.includes(commandName))
-
-        // Handle logging and replying to command errors
-        async function commandError(userMsg, logMsg) {
-            if (userMsg) await e.reply(userMsg)
-            logMessage('command', e, (p) => {
-                p.tag('command', commandName)
-                p.tag('errortype', logMsg || userMsg)
-                return p
-            })
-        }
-
-        if (!command) return await commandError('Command doesnt exist, check `commands` and try again', 'does not exist');
-        // Ignore problems if command is eval, user is bot admin, and server is vibot info
-        try { if (require('./settings.json').botOwners.includes(e.member.id) && e.guild.id == '739623118833713214') return await command.execute(e, args, this.#bot, getDB(e.guild.id)) } catch (er) { console.log('botOwners not found in settings.json. Update settings with new items from settings_template') }
         // Validate the command is enabled
-        if (!this.#bot.settings[e.guild.id].commands[command.name]) return await commandError('This command is disabled', 'disabled');
+        if (!this.#bot.settings[e.guild.id].commands[command.name]) return await this.#commandError(e, commandName, 'This command is disabled', 'disabled')
+
         // Validate the command is not disabled during restart if a restart is pending
-        if (restarting.restarting && !command.allowedInRestart && !this.#bot.adminUsers.includes(e.member.id)) return await commandError('Cannot execute command as a restart is pending', 'pending restart')
+        if (restarting.restarting && !command.allowedInRestart && !this.#bot.adminUsers.includes(e.member.id)) return await this.#commandError(e, commandName, 'Cannot execute command as a restart is pending', 'pending restart')
+
         // Validate the user has permission to use the command
-        if (!e.guild.roles.cache.get(this.#bot.settings[e.guild.id].roles[command.role])) return await commandError('Permissions not set up for this commands role', 'permissions not setup')
+        if (!e.guild.roles.cache.get(this.#bot.settings[e.guild.id].roles[command.role])) return await this.#commandError(e, commandName, 'Permissions not set up for this commands role', 'permissions not setup')
 
-        if (!this.commandPermitted(e.member, e.guild, command)) return await commandError('You do not have permission to use this command', 'no permission')
+        if (!this.#commandPermitted(e.member, e.guild, command)) return await this.#commandError(e, commandName, 'You do not have permission to use this command', 'no permission')
 
-        if (command.requiredArgs && command.requiredArgs > argCount) return await commandError(`Command Entered incorrecty. \`${this.#botSettings.prefix}${command.name} ${argString(command.args)}\``, 'invalid syntax')
+        if (command.requiredArgs && command.requiredArgs > argCount) return await this.#commandError(e, commandName, `Command Entered incorrecty. \`${this.#botSettings.prefix}${command.name} ${argString(command.args)}\``, 'invalid syntax')
+    }
 
-        if (command.cooldown) {
-            if (this.#cooldowns.get(command.name)) {
-                if (Date.now() + (command.cooldown * 1000) < Date.now()) this.#cooldowns.delete(command.name)
-                else return await commandError(null, 'cooldown')
-            } else this.#cooldowns.set(command.name, Date.now())
-            setTimeout(() => { this.#cooldowns.delete(command.name) }, command.cooldown * 1000)
-        }
-
+    async #runCommand(e, isInteraction, command, commandName, args) {
         const point = genPoint('commandexecution', e).tag('command', commandName)
         try {
             const db = getDB(e.guild.id)
-            if (isInteraction && command.slashCommandExecute) {
-                command.slashCommandExecute(e, this.#bot, db)
-            } else {
+            if (isInteraction && command.slashCommandExecute) command.slashCommandExecute(e, this.#bot, db)
+            else {
                 // Add the shim for options
-                if (!isInteraction && typeof (command.args) == 'object') {
+                if (!isInteraction && typeof(command.args) == 'object') {
                     try {
                         const lco = new LegacyCommandOptions(command.args, e, command.varargs)
                         Object.defineProperty(e, 'options', {
@@ -170,15 +145,17 @@ class MessageManager {
                             }
                         })
                     } catch (err) {
+                        // can't seem to reason around reducing depth here
+                        // eslint-disable-next-line max-depth
                         if (err instanceof LegacyParserError) return e.replyUserError(err.message)
-                        throw (err)
+                        throw err
                     }
                 }
                 const start = Date.now()
                 await command.execute(e, args, this.#bot, db)
                 point.intField('duration', Date.now() - start)
             }
-            db.query(`INSERT INTO commandusage (command, userid, guildid, utime) VALUES ('${command.name}', '${e.member.id}', '${e.guild.id}', '${Date.now()}')`);
+            db.query('INSERT INTO commandusage (command, userid, guildid, utime) VALUES (?, ?, ?, ?)', [command.name, e.member.id, e.guild.id, Date.now()])
             if (isInteraction) {
                 CommandLogger.logInteractionCommand(e, this.#bot)
             } else {
@@ -186,11 +163,47 @@ class MessageManager {
             }
         } catch (er) {
             ErrorLogger.log(er, this.#bot, e.guild)
-            e.reply('Issue executing the command, check `;commands` and try again');
+            e.reply('Issue executing the command, check `;commands` and try again')
             point.stringField('error', er.name)
         } finally {
             writePoint(point)
         }
+    }
+
+    /**
+     * Runs the command processing pipeline including parsing, state checks, permissions checks, etc.
+     * @param {Discord.Message | Discord.Interaction} e
+     * @param {boolean} isInteraction
+     * @returns
+     */
+    async handleCommand(e, isInteraction) {
+        if (isInteraction && !this.#bot.settings[e.guild.id]) return
+
+        const args = e.getArgs?.() ?? e.content.slice(this.#prefix.length).split(/ +/gi)
+        const commandName = e.commandName ?? args.shift()
+        const argCount = e.options?.length ?? args.length
+
+        // Get the command
+        const command = this.#bot.commands.get(commandName) || this.#bot.commands.find(cmd => cmd.alias && cmd.alias.includes(commandName))
+
+        // Ignore problems if command is eval, user is bot admin, and server is vibot info
+        try {
+            if (require('./settings.json').botOwners.includes(e.member.id) && e.guild.id == '739623118833713214') return await command.execute(e, args, this.#bot, getDB(e.guild.id))
+        } catch (er) {
+            console.log('botOwners not found in settings.json. Update settings with new items from settings_template')
+        }
+
+        const validationResult = await this.#validateCommand(e, command, commandName, argCount)
+        if (validationResult) return validationResult
+
+        if (command.cooldown) {
+            if (this.#cooldowns.get(command.name)) {
+                if (Date.now() + (command.cooldown * 1000) < Date.now()) this.#cooldowns.delete(command.name)
+                else return await this.#commandError(e, commandName, null, 'cooldown')
+            } else this.#cooldowns.set(command.name, Date.now())
+            setTimeout(() => { this.#cooldowns.delete(command.name) }, command.cooldown * 1000)
+        }
+        return await this.#runCommand(e, isInteraction, command, commandName, args)
     }
 
     /**
@@ -200,16 +213,16 @@ class MessageManager {
      */
     async dmHandler(message) {
         if (verification.checkActive(message.author.id)) return
-        let cancelled = false;
+        let cancelled = false
         const statsTypos = ['stats', 'satts', 'stat', 'status', 'sats', 'stata', 'stts', 'stas']
         if (statsTypos.includes(message.content.split(' ')[0].replace(/[^a-z0-9]/gi, '').toLowerCase())) {
-            let guild;
+            let guild
             this.#bot.guilds.cache.forEach(g => {
                 if (g.members.cache.get(message.author.id)) {
-                    guild = g;
+                    guild = g
                 }
             })
-            if (!guild) cancelled = true;
+            if (!guild) cancelled = true
             this.#logCommand(guild, message)
             if (!cancelled) {
                 try {
@@ -219,13 +232,13 @@ class MessageManager {
                 }
             }
         } else if (/^.?(pl[ea]{0,2}se?\s*)?(j[oi]{2}n|d[ra]{2}g\s*(me)?)(\s*pl[ea]{0,2}se?)?$/i.test(message.content)) {
-            const guild = await this.getGuild(message).catch(er => cancelled = true)
+            const guild = await this.getGuild(message).catch(() => { cancelled = true })
             this.#logCommand(guild, message)
             if (!cancelled) {
-                require('./commands/joinRun').dmExecution(message, message.content.split(/\s+/), this.#bot, getDB(guild.id), guild, this.#tokenDB);
+                require('./commands/joinRun').dmExecution(message, message.content.split(/\s+/), this.#bot, getDB(guild.id), guild, this.#tokenDB)
             }
         } else {
-            if (message.content.replace(/[^0-9]/g, '') == message.content) return;
+            if (message.content.replace(/[^0-9]/g, '') == message.content) return
             const args = message.content.split(/ +/)
             const commandName = args.shift().toLowerCase().replace(this.#prefix, '')
             const command = this.#bot.commands.get(commandName) || this.#bot.commands.find(c => c.alias && c.alias.includes(commandName))
@@ -233,23 +246,20 @@ class MessageManager {
             else if (command.dms) {
                 let guild
                 if (command.dmNeedsGuild) {
-                    guild = await this.getGuild(message).catch(er => cancelled = true)
+                    guild = await this.getGuild(message).catch(() => { cancelled = true })
                     this.#logCommand(guild, message)
                 }
 
                 if (!cancelled) {
-                    if (!command.dmNeedsGuild) command.dmExecution(message, args, this.#bot, null, guild, this.#tokenDB)
-                    else {
-                        const member = guild.members.cache.get(message.author.id)
-                        if (member.roles.highest.position < guild.roles.cache.get(this.#bot.settings[guild.id].roles[command.role]).position && !this.#bot.adminUsers.includes(message.member.id)) {
-                            this.#sendModMail(message);
-                        } else command.dmExecution(message, args, this.#bot, getDB(guild.id), guild, this.#tokenDB)
-                    }
+                    const db = command.dmNeedsGuild ? getDB(guild.id) : null
+                    const member = guild.members.cache.get(message.author.id)
+                    const role = guild.roles.cache.get(this.#bot.settings[guild.id].roles[command.role])
+
+                    if (member.roles.highest.position < role.position && !this.#bot.adminUsers.includes(member.id)) this.#sendModMail(message)
+                    else command.dmExecution(message, args, this.#bot, db, guild, this.#tokenDB)
                 }
             } else message.channel.send('This command does not work in DM\'s. Please use this inside of a server')
-
         }
-
     }
 
     async #logCommand(guild, message) {
@@ -277,7 +287,6 @@ class MessageManager {
                 modmail.sendModMail(message, guild, this.#bot, getDB(guild.id))
                 return confirmMessage.delete()
             }
-
             return confirmMessage.delete()
         })
 
@@ -291,23 +300,23 @@ class MessageManager {
      */
     async autoMod(message) {
         const settings = this.#bot.settings[message.guild.id]
-        if (!settings || !settings.backend.automod) return;
+        if (!settings || !settings.backend.automod) return
         if (!message.member.roles.highest || message.member.roles.highest.position >= message.guild.roles.cache.get(settings.roles.eventrl).position) return
-        if (message.mentions.roles.size != 0) mute('Pinging Roles', 2);
+        if (message.mentions.roles.size != 0) mute('Pinging Roles', 2)
 
         function mute(reason, time) {
             // Time: 1=1 hour, 2=1 day
-            let timeString; let timeValue;
+            let timeString; let timeValue
             if (time == 1) {
-                timeString = '1 Hour';
+                timeString = '1 Hour'
                 timeValue = 3600000
             } else if (time == 2) {
-                timeString = '1 Day';
+                timeString = '1 Day'
                 timeValue = 86400000
             }
 
             message.member.roles.add(settings.roles.muted)
-                .then(() => this.#bot.dbs[message.guild.id].query(`INSERT INTO mutes (id, guildid, muted, reason, modid, uTime) VALUES ('${message.author.id}', '${message.guild.id}', true, '${reason}','${this.#bot.user.id}', '${Date.now() + timeValue}')`))
+                .then(() => this.#bot.dbs[message.guild.id].query('INSERT INTO mutes (id, guildid, muted, reason, modid, uTime) VALUES (?, ?, ?, ?, ?, ?)', [message.author.id, message.guild.id, true, reason, this.#bot.user.id, Date.now() + timeValue]))
                 .then(() => message.author.send(`You have been muted in \`${message.guild.name}\` for \`${reason}\`. This will last for \`${timeString}\``))
                 .then(() => {
                     const modlog = message.guild.channels.cache.get(settings.channels.modlog)
@@ -342,8 +351,8 @@ class MessageManager {
             .setColor('#fefefe')
             .setDescription('Press Cancel if you don\'t wanna proceed')
         const guildSelectionMessage = await message.channel.send({ embeds: [guildSelectionEmbed] })
-        const choice = await guildSelectionMessage.confirmList(guildNames, message.author.id);
-        guildSelectionMessage.delete();
+        const choice = await guildSelectionMessage.confirmList(guildNames, message.author.id)
+        guildSelectionMessage.delete()
         if (!choice || choice == 'Cancelled') return false
 
         for (const i in guilds) {
@@ -364,4 +373,4 @@ class MessageManager {
     }
 }
 
-module.exports = { MessageManager };
+module.exports = { MessageManager }
