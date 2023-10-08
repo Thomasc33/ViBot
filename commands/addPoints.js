aef13acf17ee5c85996e9b7fd9ca77d3724bded1
husky-rotmg <38387144+kerr-andrew@users.noreply.github.com>
2023-10-07 20:06:24 -0400

    ESLint addPoints

--- commands/addPoints.js
+++ commands/addPoints.js
@@ -1,7 +1,6 @@
 const botSettings = require('../settings.json')
 const ErrorLogger = require('../lib/logError')
-const Discord = require('discord.js')
-const SlashArgType = require('discord-api-types/v10').ApplicationCommandOptionType;
+const SlashArgType = require('discord-api-types/v10').ApplicationCommandOptionType
 const { slashArg, slashChoices, slashCommandJSON } = require('../utils.js')
 
 module.exports = {
@@ -15,9 +14,9 @@ module.exports = {
         slashArg(SlashArgType.String, 'type', {
             description: 'Type of points to add',
             choices: slashChoices({
-                'Streaming': 'stream',
-                'Puri': 'priest',
-                'Trickster': 'trickster'
+                Streaming: 'stream',
+                Puri: 'priest',
+                Trickster: 'trickster'
             })
         }),
         slashArg(SlashArgType.User, 'user', {
@@ -25,34 +24,30 @@ module.exports = {
         })
     ],
     getSlashCommandData(guild) {
-        let settings = guild.client.settings[guild.id]
+        const settings = guild.client.settings[guild.id]
         if (!settings || !settings.backend.points || !settings.commands.addpoints) return undefined
         return slashCommandJSON(this, guild)
     },
     async execute(message, args, bot, db) {
-        let settings = bot.settings[message.guild.id]
-        if (!settings || !settings.backend.points) return
-        let type
-        let command = message.content.substring(botSettings.prefix.length, message.content.length).split(/ +/)[0].toLowerCase()
-        if (command == this.name) type = args.shift()
-        else type = command
-        let member = message.guild.members.cache.get(args[0])
-        if (!member) member = message.mentions.members.first()
-        if (!member) member = message.guild.members.cache.filter(user => user.nickname != null).find(nick => nick.nickname.replace(/[^a-z|]/gi, '').toLowerCase().split('|').includes(args[0].toLowerCase()));
+        const settings = bot.settings[message.guild.id]
+        if (!settings?.backend?.points) return message.channel.send('This server does not have points functionality enabled')
+        const command = message.content.substring(botSettings.prefix.length, message.content.length).split(/ +/)[0].toLowerCase()
+        const type = command == this.name ? args.shift() : command
+
+        const member = message.guild.findMember(args[0])
         if (!member) return message.channel.send(`${args[0]} not found`)
-        let points
+        let points = 0
         switch (type) {
-            case 'stream': points = settings.points.o3streaming; break;
-            case 'priest': points = settings.points.o3puri; break;
-            case 'trickster': points = settings.points.o3trickster; break;
+            case 'stream': points = settings.points.o3streaming; break
+            case 'priest': points = settings.points.o3puri; break
+            case 'trickster': points = settings.points.o3trickster; break
             default: return message.reply(`${type} not recognized`)
         }
-        db.query(`UPDATE users SET points = points + ${points} WHERE id = '${member.id}'`, (err, rows) => {
+        db.query('UPDATE users SET points = points + ? WHERE id = ?', [points, member.id], err => {
             if (err) {
-                interaction.replyInternalError("Error adding points.")
+                message.replyInternalError('Error adding points.')
                 ErrorLogger.log(err, bot, message.guild)
-            }
-            else message.replySuccess(`Added ${points} (${type}) points to ${member.user.tag}`)
+            } else message.replySuccess(`Added ${points} (${type}) points to ${member.user.tag}`)
         })
-    },
+    }
 }
