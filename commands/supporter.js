b243b5e5e675c92351a832a1e653259195c4d5e4
Raghav Viswakumar <rv519@ic.ac.uk>
2023-10-13 21:16:05 +0100

    Updated supporter.js
    + extensions.js has a supporterHierarchy command for roles
    + supporter.js now limits users from checking others
    + supporter.js has improved UI
    + supporter.js shows all available supporter roles for non-supporters

--- commands/supporter.js
+++ commands/supporter.js
@@ -16,30 +16,53 @@ module.exports = {
     getSlashCommandData(guild) { return slashCommandJSON(this, guild) },
     async execute(message, args, bot, db) {
         const botSettings = bot.settings[message.guild.id]
-        const member = message.guild.findMember(args.join('')) || message.mentions.members.first() || message.member
+        if (message.member.roles.highest.position < message.guild.roles.cache.get(botSettings.roles.minimumSupporterCheckRole) && args.length > 0) return await message.reply({ embeds: [createEmbed(message, `You do not have the required role ${message.guild.roles.cache.get(botSettings.roles.minimumSupporterCheckRole)} to use this command.`, null)], ephemeral: true })
+        const member = message.guild.findMember(args.join('')) || message.member
         const supporterRoles = botSettings.lists.perkRoles.map(role => message.guild.roles.cache.get(botSettings.roles[role]))
-        if (!member.roles.cache.hasAny(...supporterRoles.map(role => role?.id))) {
-            return await message.reply({ embeds: [createEmbed(message, `This user \`${member.displayName}\` ${member} does not have one of the required Supporter Roles ${supporterRoles.join(', ')}.`, null)], ephemeral: true })
+        const supporterError = []
+
+        let rolesText = ''
+        let cooldownText = ''
+        let useText = ''
+        for (const role of supporterRoles) {
+            const supporterNumber = role.supporterHierarchy(botSettings)
+            const cooldown = botSettings.supporter[`supporterCooldownSeconds${supporterNumber}`]
+            const uses = botSettings.supporter[`supporterUses${supporterNumber}`]
+            if (supporterNumber == 0) {supporterError.push(role); continue}
+            if (!botSettings.supporter[`supporterCooldownSeconds${supporterNumber}`]) {supporterError.push(role); continue}
+            if (!botSettings.supporter[`supporterUses${supporterNumber}`]) {supporterError.push(role); continue}
+            rolesText += `${role}\n`
+            cooldownText += (cooldown < 86400) ? ((cooldown < 3600) ? `\`${(cooldown / 60).toFixed(0)}\` Minutes\n` : `\`${(cooldown / 3600).toFixed(0)}\` Hours\n`) : `\`${(cooldown / 86400).toFixed(0)}\` Days\n`
+            useText += `\`${uses}\`\n`
         }
+        if (supporterError.length > 0) return await message.reply({ embeds: [createEmbed(message, `The server has incorrectly defined one of the following Supporter Roles:\n${supporterRoles.join(', ')}`, null)], ephemeral: true })
+        if (member.roles.highest.position < message.guild.roles.cache.get(botSettings.roles.minimumSupporterCheckRole)) return await message.reply({ embeds: [createEmbed(message, `You do not have the required role ${message.guild.roles.cache.get(botSettings.roles.minimumSupporterCheckRole)} to use this command.`, null)], ephemeral: true })
+
+        const embed = createEmbed(message, `Available Supporter Perks for ${member}\n`, null)
+        embed.setAuthor({ name: member.displayName, iconURL: member.user.displayAvatarURL({ dynamic: true }) })
+        embed.setTitle('Current Uses')
+        if (!member.roles.cache.hasAny(...supporterRoles.map(role => role?.id))) {
+            embed.data.description = `Supporter Perks are unavailable for ${member}\nAvailable Supporter Role information for this server`
+            embed.addFields({ name: 'Supporter Roles', value: rolesText, inline: true },
+                { name: 'Cooldown', value: cooldownText, inline: true },
+                { name: 'Uses', value: useText, inline: true })
+            return await message.reply({ embeds: [embed], ephemeral: true })
+        }
+
         const supporterNumber = member.supporterHierarchy(botSettings)
         const supporterRole = message.guild.roles.cache.get(member.supporterRoleHierarchy(botSettings))
-        if (!supporterRole) {
-            return await message.reply({ embeds: [createEmbed(message, `This server does not have at least one of the required Supporter Roles ${supporterRoles.join(', ')} correctly defined.`, null)], ephemeral: true })
-        }
         const cooldown = botSettings.supporter[`supporterCooldownSeconds${supporterNumber}`]
         const uses = botSettings.supporter[`supporterUses${supporterNumber}`]
         const lastUseCheck = Date.now() - (cooldown * 1000)
         const [rows,] = await db.promise().query('SELECT * FROM supporterusage WHERE guildid = ? AND userid = ? AND utime > ?', [message.guild.id, member.id, lastUseCheck])
-        let cooldownText = ''
-        if (cooldown < 3600) cooldownText = `\`${(cooldown / 60).toFixed(0)}\` minutes`
-        else cooldownText = `\`${(cooldown / 3600).toFixed(0)}\` hours`
-        const embed = createEmbed(message, `Your highest Supporter Role is ${supporterRole}.\nYour Supporter Perk usage is currently at \`${rows.length}\` out of \`${uses}\` uses and regenerates after ${cooldownText}.\n`, null)
-        embed.setTitle(`Supporter Usage for \`${member.displayName}\``)
+        embed.setThumbnail(supporterRole?.iconURL({ dynamic: true }))
+        embed.setColor(supporterRole?.color)
+        embed.addFields({ name: 'Supporter Role', value: `${supporterRole}`, inline: true },
+            { name: 'Next Usage', value: `${rows.length >= uses ? `<t:${(((cooldown * 1000) + parseInt(rows[0].utime)) / 1000).toFixed(0)}:f>` : '**Now**'}`, inline: true },
+            { name: 'Uses Left', value: `\`${uses - rows.length}/${uses}\`  ${(rows.length > 0) ? `(\`+1\` <t:${(((cooldown * 1000) + parseInt(rows[0].utime)) / 1000).toFixed(0)}:R>)` : ''}`, inline: true })
         if (rows.length > 0) {
-            embed.data.description += `Your next use will regenerate <t:${(((cooldown * 1000) + parseInt(rows[0].utime)) / 1000).toFixed(0)}:R>.\n`
-            let supporterUses = ''
-            for (const row of rows) supporterUses += `- <t:${(row.utime / 1000).toFixed(0)}:R>\n`
-            embed.addFields({ name: 'Uses', value: supporterUses })
+            const supporterUses = rows.map(row => `- <t:${(row.utime / 1000).toFixed(0)}:f>`).join('\n')
+            embed.addFields({ name: 'Past Usage', value: supporterUses })
         }
         await message.reply({ embeds: [embed], ephemeral: true })
     }
