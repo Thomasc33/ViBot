3b0cd021b885bb46340f0a52b28821f70ab1c7b0
Raghav Viswakumar <rv519@ic.ac.uk>
2023-10-13 17:31:06 +0100

    Updated supporter.js
    + Fixed eslint errors for supporter.js
    + Fixed UI for supporter.js

--- commands/supporter.js
+++ commands/supporter.js
@@ -1,7 +1,6 @@
-const Discord = require('discord.js')
-const SlashArgType = require('discord-api-types/v10').ApplicationCommandOptionType;
+const SlashArgType = require('discord-api-types/v10').ApplicationCommandOptionType
 const { createEmbed } = require('../lib/extensions.js')
-const { slashArg, slashChoices, slashCommandJSON } = require('../utils.js')
+const { slashArg, slashCommandJSON } = require('../utils.js')
 
 module.exports = {
     name: 'supporter',
@@ -11,15 +10,15 @@ module.exports = {
     args: [
         slashArg(SlashArgType.User, 'user', {
             required: false,
-            description: 'Supporter to check usage for',
+            description: 'Supporter to check usage for'
         })
     ],
     getSlashCommandData(guild) { return slashCommandJSON(this, guild) },
     async execute(message, args, bot, db) {
         const botSettings = bot.settings[message.guild.id]
-        const member = message.guild.members.cache.get(args[0]) || message.mentions.members.first() || message.member
+        const member = message.guild.findMember(args.join('')) || message.mentions.members.first() || message.member
         const supporterRoles = botSettings.lists.perkRoles.map(role => message.guild.roles.cache.get(botSettings.roles[role]))
-        if (!member.roles.cache.hasAny(...supporterRoles.map(role => role.id))) {
+        if (!member.roles.cache.hasAny(...supporterRoles.map(role => role?.id))) {
             return await message.reply({ embeds: [createEmbed(message, `This user \`${member.displayName}\` ${member} does not have one of the required Supporter Roles ${supporterRoles.join(', ')}.`, null)], ephemeral: true })
         }
         const supporterNumber = member.supporterHierarchy(botSettings)
@@ -27,27 +26,21 @@ module.exports = {
         if (!supporterRole) {
             return await message.reply({ embeds: [createEmbed(message, `This server does not have at least one of the required Supporter Roles ${supporterRoles.join(', ')} correctly defined.`, null)], ephemeral: true })
         }
-        let cooldown = botSettings.supporter[`supporterCooldownSeconds${supporterNumber}`]
-        let uses = botSettings.supporter[`supporterUses${supporterNumber}`]
-        let lastUseCheck = Date.now() - (cooldown * 1000)
-        let [rows,] = await db.promise().query(`SELECT * FROM supporterusage WHERE guildid = ? AND userid = ? AND utime > ?`, [message.guild.id, member.id, lastUseCheck])
-        let cooldown_text = ''
-        if (cooldown < 3600) cooldown_text = `\`${(cooldown/60).toFixed(0)}\` minutes`
-        else cooldown_text = `\`${(cooldown/3600).toFixed(0)}\` hours`
-        const embed = createEmbed(message, `Your highest Supporter Role is ${supporterRole}.\nYour perks are limited to \`${uses}\` times every ${cooldown_text}.\n`, null)
+        const cooldown = botSettings.supporter[`supporterCooldownSeconds${supporterNumber}`]
+        const uses = botSettings.supporter[`supporterUses${supporterNumber}`]
+        const lastUseCheck = Date.now() - (cooldown * 1000)
+        const [rows,] = await db.promise().query('SELECT * FROM supporterusage WHERE guildid = ? AND userid = ? AND utime > ?', [message.guild.id, member.id, lastUseCheck])
+        let cooldownText = ''
+        if (cooldown < 3600) cooldownText = `\`${(cooldown / 60).toFixed(0)}\` minutes`
+        else cooldownText = `\`${(cooldown / 3600).toFixed(0)}\` hours`
+        const embed = createEmbed(message, `Your highest Supporter Role is ${supporterRole}.\nYour Supporter Perk usage is currently at \`${rows.length}\` out of \`${uses}\` uses and regenerates after ${cooldownText}.\n`, null)
         embed.setTitle(`Supporter Usage for \`${member.displayName}\``)
-        switch (rows.length) {
-            case (rows.length > uses):
-                embed.description += `Your next use is available <t:${(((cooldown*1000)+parseInt(rows[0].utime))/1000).toFixed(0)}:R>`
-            case (rows.length > 0):
-                embed.description += `You have used your perks \`${rows.length}\` times in the last ${cooldown_text}.\n`
-                for (const row of rows) embed.addFields({ name: ' ', value: `Used Supporter Perks <t:${(row.utime/1000).toFixed(0)}:R>` })
-                break
-            default:
-                embed.description += `You have not used your perks in the last ${cooldown_text}.\n`
-                break
-
+        if (rows.length > 0) {
+            embed.data.description += `Your next use will regenerate <t:${(((cooldown * 1000) + parseInt(rows[0].utime)) / 1000).toFixed(0)}:R>.\n`
+            let supporterUses = ''
+            for (const row of rows) supporterUses += `- <t:${(row.utime / 1000).toFixed(0)}:R>\n`
+            embed.addFields({ name: 'Uses', value: supporterUses })
         }
-        await message.reply({ embeds: [embed], ephemeral: true})
-    },
+        await message.reply({ embeds: [embed], ephemeral: true })
+    }
 }
