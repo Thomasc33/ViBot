4e7c85be0b51bc2e7b3f3a2111c3ee1f71a054bb
tro2 <62850247+tro2@users.noreply.github.com>
2024-01-16 20:28:14 -0800

    refactor sepukku command
    Bring seppuku in line with lintr

--- commands/seppuku.js
+++ commands/seppuku.js
@@ -1,4 +1,6 @@
+// eslint-disable-next-line no-unused-vars
 const Discord = require('discord.js')
+// eslint-disable-next-line no-unused-vars
 const Suspend = require('./suspend')
 
 module.exports = {
@@ -6,12 +8,13 @@ module.exports = {
     role: 'eventrl',
     description: '死ぬ',
     async execute(message, args, bot, db) {
-        let settings = bot.settings[message.guild.id]
-        let suspendedRole = settings.roles.tempsuspended
-        message.channel.send(`死ぬ!`)
-        let time = 300000 // 5 min
-        let reason = 'seppuku'
-        let userRolesString = '', userRoles = []
+        const settings = bot.settings[message.guild.id]
+        const suspendedRole = settings.roles.tempsuspended
+        message.channel.send('死ぬ!')
+        const time = 300000 // 5 min
+        const reason = 'seppuku'
+        let userRolesString = ''
+        const userRoles = []
         message.member.roles.cache.each(r => {
             if (!r.managed) return
             if (settings.lists.discordRoles.map(role => settings.roles[role]).includes(r.id)) return
@@ -20,6 +23,8 @@ module.exports = {
         })
         await message.member.roles.remove(userRoles)
         setTimeout(() => { message.member.roles.add(suspendedRole); }, 1000)
-        db.query(`INSERT INTO suspensions (id, guildid, suspended, uTime, reason, modid, roles, logmessage) VALUES ('${message.member.id}', '${message.guild.id}', true, '${Date.now() + time}', ${db.escape(reason)}, '${message.author.id}', '${userRolesString}', '${message.id}');`)
+        // db.query(`INSERT INTO suspensions (id, guildid, suspended, uTime, reason, modid, roles, logmessage) VALUES ('${message.member.id}', '${message.guild.id}', true, '${Date.now() + time}', ${db.escape(reason)}, '${message.author.id}', '${userRolesString}', '${message.id}');`)
+        db.query('INSERT INTO suspensions (id, guildid, suspended, uTime, reason, modid, roles, logmessage) VALUES (?, ?, true, ?, ?, ?, ?, ?);',
+            [message.member.id, message.guild.id, Date.now() + time, db.escape(reason), message.author.id, userRolesString, message.id])
     }
 }
