732bae48df0fa89638557dd63713af89ef7428fa
David Zhang <davidwz2003@yahoo.com>
2024-01-31 00:07:26 -0500

    formatting

--- commands/suspend.js
+++ commands/suspend.js
@@ -106,14 +106,9 @@ module.exports = {
                         .setFooter({ text: `Unsuspending at ` })
                         .setTimestamp(Date.now() + time);
                     if (overwrite) {
-
                         db.query(`SELECT reason FROM suspensions WHERE id = '${member.id}' AND suspended = true`, async (err, rows) => {
-
                             const new_suspender = message.guild.members.cache.get(message.author.id).nickname
-                            db.query(`UPDATE suspensions SET suspended = false, reason = '${'(overwritten by ' + new_suspender + ') - ' + reason}' WHERE id = '${member.id}' AND suspended = true`, async (e, _) => {
-                                if(e) console.log(e)
-                            })
-
+                            db.query(`UPDATE suspensions SET suspended = false, reason = '${'(overwritten by ' + new_suspender + ') - ' + reason}' WHERE id = '${member.id}' AND suspended = true`)
                             embed.data.fields[3].value = `Overwritten suspensions. Roles the same as prior suspension`
                             suspensionLog.send({ embeds: [embed] }).then(member.user.send({ embeds: [embed] }).catch(() => {}))
                         })
