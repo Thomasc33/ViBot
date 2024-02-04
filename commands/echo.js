c44fff616609a87377a3965bb9ee4962521b081f
tro2 <62850247+tro2@users.noreply.github.com>
2024-02-04 15:49:30 -0800

    update echo.js
    eslint changes to echo + Initial draft of echo slash command

--- commands/echo.js
+++ commands/echo.js
@@ -1,9 +1,24 @@
+const { slashArg, slashCommandJSON } = require('../utils.js');
+const SlashArgType = require('discord-api-types/v10').ApplicationCommandOptionType;
+
 module.exports = {
     name: 'echo',
     role: 'moderator',
-    description: 'posts the message given',
-    execute(message, args, bot) {
-        message.channel.send(message.content.substring(6, message.content.length))
-        message.delete()
+    description: 'Posts the message given',
+    varargs: true,
+    args: [
+        slashArg(SlashArgType.String, 'message', {
+            description: 'The message to echo'
+        })
+    ],
+    getSlashCommandData(guild) {
+        return slashCommandJSON(this, guild);
+    },
+    async execute(message) {
+        await message.channel.send(message.content.substring(6, message.content.length));
+        await message.delete();
+    },
+    async slashCommandExecute(interaction) {
+        await interaction.channel.send(interaction.options.getString('message'));
     }
-}
+};
