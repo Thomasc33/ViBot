8dfb87ff1648401e8ccec618c5bdb6595c8dc0bb
tro2 <62850247+tro2@users.noreply.github.com>
2024-02-05 14:15:10 -0800

    Update parse.js
    push for slash arg testing

--- commands/parse.js
+++ commands/parse.js
@@ -2,48 +2,72 @@ const SlashArgType = require('discord-api-types/v10').ApplicationCommandOptionTy
 const { slashArg, slashCommandJSON } = require('../utils.js');
 
 module.exports = {
-    name: 'bananas',
+    name: 'parse',
     description: 'read the name',
-    alias: ['pm'],
+    alias: ['parse'],
     args: [
-        slashArg(SlashArgType.Subcommand, 'reacts', {
-            description: 'Lists reacts for the run',
-            options: [
-                slashArg(SlashArgType.String, 'raidId', {
-                    required: false,
-                    description: 'The id of the raid you wish to parse'
-                })
-            ]
-        }),
-        slashArg(SlashArgType.Subcommand, 'simple_VC', {
-            description: 'Checks vc against /who only',
-            options: [
-                slashArg(SlashArgType.Attachment, 'who', {
-                    required: true,
-                    description: '/who image'
-                }),
-                slashArg(SlashArgType.String, 'vcId', {
-                    required: false,
-                    description: 'vc you are trying to parse'
-                })
-            ]
-        }),
         slashArg(SlashArgType.Subcommand, 'members', {
             description: 'regular old parse',
             options: [
-                slashArg(SlashArgType.Attachment, 'who', {
+                slashArg(SlashArgType.Attachment, 'players', {
                     required: true,
                     description: '/who image'
                 }),
-                slashArg(SlashArgType.String, 'raidId', {
+                slashArg(SlashArgType.String, 'raid', {
                     required: false,
-                    description: 'The id of the raid you are trying to parse'
+                    autocomplete: true,
+                    description: 'The raid to parse'
                 }),
+            ]
+        }),
+        slashArg(SlashArgType.Subcommand, 'reacts', {
+            description: 'Lists reacts for the run',
+            options: [
+                slashArg(SlashArgType.String, 'raid', {
+                    autocomplete: true,
+                    required: false,
+                    description: 'The raid to parse'
+                })
+            ]
+        }),
+        slashArg(SlashArgType.Subcommand, 'simple', {
+            description: 'Checks vc against /who only',
+            options: [
+                slashArg(SlashArgType.Attachment, 'players', {
+                    required: true,
+                    description: '/who image'
+                }),
+                slashArg(SlashArgType.String, 'vc_id', {
+                    required: false,
+                    description: 'The vc to parse'
+                })
             ]
         })
     ],
     getSlashCommandData(guild) { return slashCommandJSON(this, guild); },
     role: 'eventrl',
-    async execute() { },
-    async slashCommandExecute() { }
+    async autocomplete(interaction) {
+        const focusedValue = interaction.options.getFocused().trim().toLowerCase();
+        switch (focusedValue.name) {
+            case 'raid':
+                break;
+            default:
+        }
+    },
+    async execute(message/* , bot, db */) {
+        const action = message.options.getSubcommand();
+        switch (action) {
+            case 'members':
+                console.log(message.options[0].options);
+                await message.reply('members');
+                break;
+            case 'reacts':
+                await message.reply('reacts');
+                break;
+            case 'simple':
+                await message.reply('simple');
+                break;
+            default:
+        }
+    }
 };
