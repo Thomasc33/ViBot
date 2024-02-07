13e8abbecbcdd37939200b6fa7dec391f784add9
husky-rotmg <70654625+husky-rotmg@users.noreply.github.com>
2024-02-07 01:07:54 -0500

    Several refactors
    # suspendremove.js
    - Now will also show active suspension if there is one and allow you to unsuspend and remove the suspension from DB at the same time
    - Multiple embeds sent for selecting which suspension if they don't all fit on one embed
    - Added sorting so the most recent suspensions should show at the top (probably)
    
    # unsuspend.js
    I might've rewrote most of the command oops
    - Added `processUnsuspend` function to make unsuspending more consistent
    - Now properly applies unverified role (probably, currently untested) if impossible to verify nicknameless user's roles
    - Should not fail for anything with managed roles
    - Unsuspending permanent suspensions has a confirmation
    - Once config is up to date, added configurable `settings.rolePermissions.unsuspendPermanentSuspension` if a guild wants to make permanent suspensions a different role to manage.
    - Embed-ified all replies
    
    # suspend.js
    - Moved query to parameterized query so `reason` can be properly sent without weird escaping
    - Capped reason at 255 characters due to db limit

--- commands/suspendremove.js
+++ commands/suspendremove.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const SlashArgType = require('discord-api-types/v10').ApplicationCommandOptionType;
 const { slashArg, slashCommandJSON } = require('../utils.js');
-
+const { processUnsuspend } = require('./unsuspend.js');
 module.exports = {
     name: 'suspendremove',
     slashCommandName: 'suspendremove',
@@ -19,23 +19,60 @@ module.exports = {
      * @param {Discord.Client} bot
      * @param {import('mysql2').Connection} db
      */
+    // eslint-disable-next-line complexity
     async execute(message, args, bot, db) {
+        /** @type {import('../data/guildSettings.701483950559985705.cache.json')} */
         const settings = bot.settings[message.guild.id];
         const modlogs = message.guild.channels.cache.get(settings.channels.modlogs);
         if (!modlogs) return message.channel.send('The modlogs channel is not configured for this server.');
 
+        /** @type {Discord.GuildMember} */
         const member = message.options.getMember('user');
         if (!member) return message.channel.send('Member not found. Please try again');
-        const [rows] = await db.promise().query('SELECT * FROM suspensions WHERE id = ? AND suspended = 0', [member.user.id]);
+        const [rows] = await db.promise().query('SELECT * FROM suspensions WHERE guildid = ? AND id = ? ORDER BY suspended DESC, unixTimestamp DESC, uTime DESC', [member.guild.id, member.user.id]);
 
         // Performs a check to see if the raider is currently suspended, if they are, you will not be allowed to continue and the raider stays suspended
-        if (rows.length == 0) return message.channel.send('There are no expired suspensions found for the user. Please unsuspend them and try again.');
-        const embed = new Discord.EmbedBuilder()
-            .setTitle('Suspension Removal')
-            .setColor(Discord.Colors.Blue)
-            .setDescription('**Select a suspension to remove:**\n\n' + rows.map((sus, index) => `${index + 1}. By <@!${sus.modid}> ended <t:${(parseInt(sus.uTime) / 1000).toFixed(0)}:R> at <t:${(parseInt(sus.uTime) / 1000).toFixed(0)}:f>\`\`\`${sus.reason}\`\`\``).join('\n'));
-        const rsPanel = await message.channel.send({ embeds: [embed] });
-        const choice = await rsPanel.confirmNumber(rows.length, message.member.id).catch(() => false);
+        if (rows.length == 0) {
+            const embed = new Discord.EmbedBuilder()
+                .setTitle('Suspension Removal')
+                .setColor(Discord.Colors.Red)
+                .setDescription(`${member} has no suspensions managed by ${bot.user}`);
+            return message.reply({ embeds: [embed] });
+        }
+
+        /** @type {string[]} */
+        const descriptions = rows.slice(0, 24).map((row, index) => {
+            const time = `set to end <t:${(parseInt(row.uTime) / 1000).toFixed(0)}:R>`;
+            const at = `at <t:${(parseInt(row.uTime) / 1000).toFixed(0)}:t> <t:${(parseInt(row.uTime) / 1000).toFixed(0)}:d>`;
+            const bulk = `${row.suspended ? '**' : ''}${index + 1}.${row.suspended ? ' Active**' : ''} By <@!${row.modid}> ${time} ${at}`;
+            return `${bulk}\n\`\`\`${row.reason}\`\`\``;
+        }).reduce((acc, info) => {
+            if (acc[acc.length - 1].length + info.length + 1 > 3800) acc.push('');
+            acc[acc.length - 1] += info + '\n';
+            return acc;
+        }, ['']);
+
+        const displays = [];
+        if (rows.length > 24) {
+            const issue = `\n\n**Only 24 items can be selected from**, however ${member} has had ${rows.length} suspensions. If the suspension you want to remove does not show up, please contact a developer to remove it directly.`;
+            if (descriptions[descriptions.length - 1].length + issue.length > 3800) descriptions.push('');
+            descriptions[descriptions.length - 1] += issue;
+        }
+
+        if (descriptions.length) {
+            const embed = new Discord.EmbedBuilder()
+                .setTitle('Suspension Removal')
+                .setColor(Discord.Colors.Blue);
+            for (const desc of descriptions) {
+                embed.setDescription(`**Select a suspension to remove from ${member}:**\n\n${desc}`);
+                // eslint-disable-next-line no-await-in-loop
+                displays.push(await message.channel.send({ embeds: [embed] }));
+            }
+        }
+
+        const rsPanel = displays[displays.length - 1];
+        const embed = Discord.EmbedBuilder.from(rsPanel.embeds[0].data);
+        const choice = await rsPanel.confirmNumber(Math.min(rows.length, 24), message.member.id).catch(() => false);
 
         if (choice === false) {
             embed.setTitle('Suspension Removal Cancelled').addFields({ name: 'Cancelled', value: 'This interaction has timed out.' });
@@ -51,16 +88,21 @@ module.exports = {
 
         const suspension = rows[parseInt(choice)];
         const mod = message.guild.members.cache.get(suspension.modid);
-
         embed.setDescription('What is your reason for removing the following suspension?')
-            .setFields(
-                { name: 'Member', value: `${member} \`${member.displayName}\``, inline: true },
-                { name: 'Issued By', value: mod ? `${mod} \`${mod.displayName}\`` : `<@${suspension.modid}>`, inline: true },
-                { name: '\u200b', value: '\u200b', inline: true },
-                { name: 'Suspended On', value: suspension.unixTimestamp ? `<t:${(parseInt(suspension.unixTimestamp) / 1000).toFixed(0)}:f>` : 'Unknown', inline: true },
-                { name: 'Ended At', value: suspension.perma ? 'Permanent' : `<t:${(parseInt(suspension.uTime) / 1000).toFixed(0)}:R>`, inline: true },
-                { name: 'Reason', value: suspension.reason || 'No Reason Provided' });
+            .setFields({ name: 'Member', value: `${member} \`${member.displayName}\``, inline: true },
+                { name: 'Issued By', value: mod ? `${mod} \`${mod.displayName}\`` : `<@${suspension.modid}>`, inline: true });
+        if (suspension.suspended) {
+            const suspensionRoles = [settings.roles.tempsuspended, settings.roles.permasuspended].filter(r => member.roles.cache.get(r)).map(r => `<@&${r}>`);
+            embed.addFields({ name: 'Active Suspension', value: suspensionRoles.join(' ') || 'In database only (no roles)', inline: true });
+        } else {
+            embed.addFields({ name: '\u200b', value: '\u200b', inline: true });
+        }
 
+        embed.addFields({ name: 'Suspended On', value: suspension.unixTimestamp ? `<t:${(parseInt(suspension.unixTimestamp) / 1000).toFixed(0)}:f>` : 'Unknown', inline: true },
+            { name: suspension.suspended ? 'Ends' : 'Ended', value: suspension.perma ? 'Permanent' : `<t:${(parseInt(suspension.uTime) / 1000).toFixed(0)}:R>`, inline: true },
+            { name: 'Reason', value: suspension.reason || 'No Reason Provided' });
+
+        displays.forEach(msg => msg != rsPanel && msg.deletable && msg.delete());
         await rsPanel.edit({ embeds: [embed], components: [] });
         const reason = await rsPanel.channel.next(null, null, message.author.id).catch(err => {
             embed.setTitle('Suspension Removal Cancelled')
@@ -88,6 +130,8 @@ module.exports = {
         }
 
         await db.promise().query('DELETE FROM suspensions WHERE id = ? AND modid = ? AND uTime = ?', [suspension.id, suspension.modid, suspension.uTime]);
+
+        await processUnsuspend(message.member, suspension, bot, db, settings, reason.content.trim() || 'No Reason Provided');
 
         embed.setDescription('Removed the following suspension')
             .setColor(Discord.Colors.Green);
