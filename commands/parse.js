639c7ea5d8949ad3fff21d9468ce5ae16f627cc2
tro2 <62850247+tro2@users.noreply.github.com>
2024-02-08 20:32:11 -0800

    Update parse.js
    Ported over Husky's parse command and started to break it up into functions

--- commands/parse.js
+++ commands/parse.js
@@ -4,6 +4,7 @@
  */
 
 const Discord = require('discord.js');
+const botSettings = require('../settings.json');
 const SlashArgType = require('discord-api-types/v10').ApplicationCommandOptionType;
 const { slashArg, slashCommandJSON } = require('../utils.js');
 
@@ -20,17 +21,13 @@ module.exports = {
                     autocomplete: true,
                     description: 'The raid to parse'
                 }),
-                slashArg(SlashArgType.String, 'vc', {
-                    required: false,
-                    description: 'The vc to parse against'
-                }),
                 slashArg(SlashArgType.Attachment, 'players', {
                     required: false,
                     description: '/who image'
                 })
             ]
         }),
-        slashArg(SlashArgType.Subcommand, 'reactlist', {
+        slashArg(SlashArgType.Subcommand, 'reacts', {
             description: 'Lists reacts for the run',
             options: [
                 slashArg(SlashArgType.String, 'raid', {
@@ -40,12 +37,12 @@ module.exports = {
                 })
             ]
         }),
-        slashArg(SlashArgType.Subcommand, 'simple', {
+        slashArg(SlashArgType.Subcommand, 'basic', {
             description: 'Checks vc against /who only',
             options: [
                 slashArg(SlashArgType.String, 'vc', {
                     required: false,
-                    description: 'The vc id to parse'
+                    description: 'The vc id to parse against (optional)'
                 }),
                 slashArg(SlashArgType.Attachment, 'players', {
                     required: false,
@@ -77,35 +74,126 @@ module.exports = {
     },
     /**
      * Executes the command.
-     * @param {Object} message - The message object.
+     * @param {Discord.Message} message - The message object.
      */
-    async execute(message, args, bot, db) {
+    async execute(message, args, bot) {
         const action = message.options.getSubcommand();
         switch (action) {
             case 'members':
-                await message.reply('members');
-                await parseMembers(message, bot, db);
+                await parseMembers(message, bot);
                 break;
             case 'reacts':
-                await message.reply('reacts');
                 break;
             case 'simple':
-                await message.reply('simple');
                 break;
             default:
         }
     }
 };
 
-async function parseMembers(message, bot, db) {
+async function parseMembers(message, bot) {
+    const playerImgURL = message.options.getAttachment('players')?.url;
+    if (!playerImgURL) return message.reply('You must provide a /who image to parse.');
     const raid = bot.afkModules[message.options.getString('raid')] || getRaid(message, bot, message.member.voice?.channelID);
-    console.log('raid object:', raid, 'raidId:', message.options.getString('raid'));
-    const vc = (raid.vcOptions == 1) ? message.options.getString('vc') : message.member.voice?.channelID;
-    const playersImage = message.options.getAttachment('players')?.url;
+    if (!raid) return; // error message already sent in getRaid()
 
-    console.log('vc id: ', vc, 'image url: ', playersImage);
+    runRaidParse(message, bot, raid, playerImgURL);
 }
 
+// async function parseReacts(message, bot) { }
+
+// async function parseSimple(message, bot) { }
+
+/**
+ *
+ * @param {Discord.Message} message - Command message object
+ * @param {Discord.Client} bot - Bot object cutosm???
+ * @param {AfkCheck} raid - the raid to parse
+ * @param {String[]} imgPlayers - the names of the players in the /who image
+ * @param {Discord.Message} parseStatusMessage - the message to edit with the status of the parse
+ * @param {Discord.Embed} parseStatusEmbed - the embed to edit with the status of the parse
+ */
+// eslint-disable-next-line complexity
+async function runRaidParse(message, bot, raid, imgPlayers, parseStatusMessage, parseStatusEmbed) {
+    parseStatusEmbed.data.fields[1].value = 'Processing Data';
+    await parseStatusMessage.edit({ embeds: [parseStatusEmbed] });
+
+    const minimumStaffRolePosition = message.guild.roles.cache.get(botSettings.roles.almostrl).position;
+
+    const raiders = imgPlayers.map(player => player.toLowerCase());
+    const members = raid.isVcless() ? raid.members : bot.channels.cache.get(raid.channel.id).members.map(m => m.id);
+
+    /** @type {{ id: string, nicknames: string[] }[]} */
+    const alts = [];
+    /** @type {string[]} */
+    const crashers = [];
+    const kick = [];
+    const find = [];
+    const otherChannel = [];
+    const allowedCrashers = [];
+
+    for (const player of raiders) {
+        const member = message.guild.findMember(player);
+        if (!member) {
+            crashers.push(player);
+            kick.push(player);
+        } else if (!members.includes(member.id)) {
+            if (member.roles.highest.position >= minimumStaffRolePosition) continue;
+
+            if (!raid.isVcless()) {
+                if (raid.members.includes(member.id)) allowedCrashers.push(member.id);
+                if (member.voice.channel) otherChannel.push(`${member}: ${member.voice.channel}`);
+                else crashers.unshift(`${member}`);
+            } else crashers.unshift(`${member}`);
+
+            kick.push(player);
+            find.push(player);
+        }
+    }
+
+    for (const memberId of members) {
+        const member = message.guild.members.cache.get(memberId);
+        if (member.roles.highest.position > minimumStaffRolePosition) continue;
+        if (!member.nickname) continue;
+        const nicknames = member.nickname.toLowerCase().replace(/[^a-z|]/gi, '').split('|');
+        if (!raiders.some(raider => nicknames.includes(raider)) && !alts.some(alt => alt.id == member.id)) alts.push({ id: member.id, nicknames });
+    }
+
+    const normalizedCrashers = crashers.map(normalizeName);
+    const normalizedAlts = alts.map(({ id, nicknames }) => ({ id, nicknames: nicknames.map(normalizeName) }));
+    const results = reassembleAndCheckNames(normalizedCrashers, normalizedAlts);
+    const [matchKeys, matchValues] = [Array.from(results.keys()), Array.from(results.values())];
+
+    const actualCrashers = crashers.filter((_, idx) => !matchValues.some(m => m.parts.includes(normalizedCrashers[idx])));
+    const possibleAlts = normalizedAlts.filter(alt => !matchKeys.some(full => alt.nicknames.some(name => full == name))).map(alt => `<@${alt.id}>`);
+    const actualKicks = kick.filter(raider => !matchValues.some(m => m.parts.includes(normalizeName(raider))));
+    const actualFind = find.filter(raider => !matchValues.some(m => m.parts.includes(normalizeName(raider))));
+
+    const embed = new Discord.EmbedBuilder()
+        .setTitle(`Parse for ${raid.afkTitle()}`)
+        .setColor('#00ff00')
+        .setDescription(`There are ${actualCrashers.length} crashers, ${possibleAlts.length} potential alts` + (raid.isVcless() ? '' : `, and ${otherChannel.length} people in other channels`))
+        .addFields({ name: 'Potential Alts', value: possibleAlts.join(', ') || 'None' });
+
+    if (!raid.isVcless()) embed.addFields({ name: 'Other Channels', value: otherChannel.join('\n') || 'None' });
+
+    embed.addFields(
+        { name: 'Crashers', value: actualCrashers.join(', ') || 'None' },
+        { name: 'Find Command', value: `\`\`\`;find ${actualFind.join(' ')}\`\`\`` },
+        { name: 'Kick List', value: actualKicks.length ? `\`\`\`${actualKicks.join(' ')}\`\`\`` : 'None' }
+    );
+
+    if (!raid.isVcless()) {
+        embed.addFields({
+            name: 'Were in VC',
+            value: `The following can use the \`reconnect\` button:\n${allowedCrashers.map(u => `<@${u}>`).join(' ')}`
+        });
+    }
+
+    await message.channel.send({ embeds: [embed] });
+    parseStatusEmbed.data.fields[1].value = 'Crasher Parse Completed. See Below. Beginning Character Parse';
+    await parseStatusMessage.edit({ embeds: [parseStatusEmbed] });
+}
 /**
  * Determines the raid via interactive modal
  * @param {Object} message - The message object.
@@ -131,7 +219,7 @@ async function getRaid(message, bot, memberVoiceChannel) {
         .setMaxValues(1);
     let text = 'Which active run would you like to parse for?';
     for (let index = 0; index < raids.length; index++) {
-        text += `\n\`\`${index+1}.\`\` ${raids[index].afkTitle()}`;
+        text += `\n\`\`${index + 1}.\`\` ${raids[index].afkTitle()}`;
         raidMenu.addOptions({ label: `${index + 1}. ${raids[index].afkTitle()}`, value: String(index) });
     }
     const { value } = await message.selectPanel(text, null, raidMenu, 30000, false, true);
@@ -151,4 +239,36 @@ async function filterRaidIds(interaction, bot) {
     const focusedValue = interaction.options.getFocused().trim().toLowerCase();
     const filteredValues = raidIdMappings.filter(raidIdMapping => raidIdMapping.name.includes(focusedValue)).slice(0, 25);
     await interaction.respond(filteredValues);
+}
+
+// Normalization function as defined earlier
+function normalizeName(name) {
+    return name.toLowerCase().replace(/\s/g, '').replace(/i/g, 'l');
+}
+
+// Function to reassemble and check split names
+/**
+ *
+ * @param {string[]} crasherNames
+ * @param {[{id: string, nicknames: string[]}]} raidMembers
+ * @returns {Map<string, { parts: string[], id: string }}
+ */
+
+function reassembleAndCheckNames(crasherNames, raidMembers) {
+    const matchedNamesMap = new Map();
+
+    const namesToCheck = crasherNames.slice(); // Copy array
+
+    while (namesToCheck.length > 0 && raidMembers.length > 0) {
+        const currentName = namesToCheck.shift();
+
+        for (let i = 0; i <= namesToCheck.length; i++) {
+            const matchedMember = raidMembers.find(({ nicknames }) => nicknames.includes(currentName + namesToCheck.slice(0, i).join('')));
+            if (matchedMember) {
+                const matchedComponents = namesToCheck.splice(0, i);
+                matchedNamesMap.set(currentName + matchedComponents.join(''), { parts: [currentName, ...matchedComponents], id: matchedMember.id });
+            }
+        }
+    }
+    return matchedNamesMap;
 }
