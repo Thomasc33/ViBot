406a1f5ba1819ad1ffc5313afc2f39dbbf847773
husky-rotmg <70654625+husky-rotmg@users.noreply.github.com>
2024-02-21 13:41:22 -0500

    Decouple 'commands' from 'bot'
    Moved `bot.commands` into `lib/commands` and refactored all references to it. Made sure to include `client.commands` too for non-direct accesses

--- commands/reload.js
+++ commands/reload.js
@@ -1,24 +1,14 @@
-const Discord = require('discord.js');
-const ErrorLogger = require('../lib/logError');
-const SlashArgType = require('discord-api-types/v10').ApplicationCommandOptionType;
-const { slashArg, slashChoices, slashCommandJSON } = require('../utils.js')
-
+const { commands } = require('../lib/commands');
 module.exports = {
     name: 'reload',
     description: 'Reloads a certain command.',
     args: '<command>',
     guildspecific: true,
     role: 'developer',
-    // args: [slashArg(SlashArgType.String, 'command', {
-    //     required: true,
-    //     description: "Any Command in Vibot"
-    // }),],
-    // getSlashCommandData(guild) {
-    //     return slashCommandJSON(this, guild)
-    // },
     async execute(message, args, bot, db) {
+        if (!bot.adminUsers.includes(message.author.id)) return;
         const commandName = args[0].toLowerCase();
-        const command = bot.commands.get(commandName);
+        const command = commands.get(commandName);
 
         // Check if command is valid
         if (!command) return message.reply(`There is no command with name \`${commandName}\`!`);
@@ -28,9 +18,9 @@ module.exports = {
         delete require.cache[require.resolve(`./${command.name}.js`)];
 
         try {
-            message.client.commands.delete(command.name);
+            commands.delete(command.name);
             const newCommand = require(`./${command.name}.js`);
-            message.client.commands.set(newCommand.name, newCommand);
+            commands.set(newCommand.name, newCommand);
             await message.reply(`Command \`${newCommand.name}\` was reloaded!`);
         } catch (error) {
             console.error(error);
